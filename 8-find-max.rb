3a603f599358acfe6cde01d1626736ecf2a6678b
Wen-Tien Chang <ihower@gmail.com>
2017-05-25 13:26:37 +0800

    Add exercise 5,6,7,8

---
+++ 8-find-max.rb
@@ -0,0 +1,14 @@
+# 题目: 使用者输入 x,y,z，请输出三个数中最大的数
+
+print "请输入一个数字x，然后按 Enter: "
+x = gets
+
+print "请输入一个数字y，然后按 Enter: "
+y = gets
+
+print "请输入一个数字z，然后按 Enter: "
+z = gets
+
+# ....
+
+puts "最大的数是 ________(x或y或z)"
