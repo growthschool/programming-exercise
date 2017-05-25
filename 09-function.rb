b0e5be2ddaab9f4ba87412f880737451518d7acb
Wen-Tien Chang <ihower@gmail.com>
2017-05-25 14:31:43 +0800

    add function 9, 10

---
+++ 09-function.rb
@@ -0,0 +1,15 @@
+# 题目: 使用者输入直角三角形的宽和高，输出三角形的面积
+
+def calculate_area(a, b)
+  # ....
+end
+
+print "请输入直角三角形的高，然后按 Enter: "
+a = gets
+
+print "请输入直角三角形的底边，然后按 Enter: "
+b = gets
+
+answer = calculate_area(a,b)
+
+puts "直角三角形的面积是: #{answer}"
---
+++ 10-function.rb
@@ -0,0 +1,19 @@
+# 题目: 使用者输入 x,y,z，请输出三个数中最大的数
+
+def find_max(x, y, z)
+end
+
+print "请输入一个数字x，然后按 Enter: "
+x = gets
+
+print "请输入一个数字y，然后按 Enter: "
+y = gets
+
+print "请输入一个数字z，然后按 Enter: "
+z = gets
+
+# ....
+
+answer = find_max(x,y,z)
+
+puts "最大的数是 #{answer}"
