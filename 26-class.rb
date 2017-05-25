36213bce6841573dcfa1cb724d540409b8984e70
Wen-Tien Chang <ihower@gmail.com>
2017-05-25 15:43:55 +0800

    Add exercise 26

---
+++ 26-class.rb
@@ -0,0 +1,16 @@
+class Person
+  # ...
+end
+
+p1 = Person.new
+p1.first_name = "Peter"
+p1.last_name = "Wang"
+p1.greet # 输出 "Hello, Peter Wang"
+
+p2 = Person.new
+p2.first_name = "William"
+p2.last_name = "Zhang"
+p2.greet # 输出 "Hello, William Zhang"
+
+
+
