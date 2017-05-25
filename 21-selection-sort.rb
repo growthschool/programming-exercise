0409327c68504dae5836446a1f129044555763c4
Wen-Tien Chang <ihower@gmail.com>
2017-05-25 22:12:31 +0800

    Add selection sort exercise

---
+++ 21-selection-sort.rb
@@ -0,0 +1,12 @@
+# 给定一数组内含数字，请实作选择排序法进行排序。
+# https://zh.wikipedia.org/wiki/选择排序
+
+def insertion_sort(arr)
+  #...
+end
+
+arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
+
+answer = insertion_sort(arr)
+
+puts answer.to_s # 应该是 [1, 7, 9, 42, 46, 46, 68, 77, 86, 91]
