9ae562913e26100bff0c8440197f685719b53507
Yammy <yammyxing@foxmail.com>
2017-05-28 19:58:41 +0800

    check linearity instead of sorting to find the max in array

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,7 +1,13 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  max = array.sort[-1]
+  max = array[0]
+  for i in array
+    if i > max
+      max = i
+    end
+  end
+  return max
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
