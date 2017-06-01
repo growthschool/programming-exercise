1e4db2c1366dd70263c9cecced0064d748780101
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-02 00:02:04 +0800

    22-missing.rb

--- 22-missing.rb
+++ 22-missing.rb
@@ -2,8 +2,13 @@
 
 def find_missing(arr)
   # ...
+  brr = []
+  for val in 0..9 do
+    brr << val unless arr.include?(val)
+  end
+  brr
 end
 
-answer = find_missing( [2,2,1,5,8,4] )
+answer = find_missing([2, 2, 1, 5, 8, 4])
 
 puts answer.to_s # 应该是 [0,3,6,7,9]
