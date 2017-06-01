d9d16a3c727d8aaadff88a0e5dccb32381afb49f
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-01 11:59:04 +0800

    08-find-max.rb

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -1,14 +1,18 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
-print "请输入一个数字x，然后按 Enter: "
+print '请输入一个数字x，然后按 Enter: '
 x = gets
 
-print "请输入一个数字y，然后按 Enter: "
+print '请输入一个数字y，然后按 Enter: '
 y = gets
 
-print "请输入一个数字z，然后按 Enter: "
+print '请输入一个数字z，然后按 Enter: '
 z = gets
 
 # ....
+xx = x.to_i
+yy = y.to_i
+zz = z.to_i
 
-puts "最大的数是 ________(x或y或z)"
+result = xx > yy ? 'x' : yy > zz ? 'y' : 'z'
+puts "最大的数是 ____#{result}____(x或y或z)"
