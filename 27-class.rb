b3c5b786c02dcd3aaf8884985596483608d42616
zhangyan <tongkusat123@gmail.com>
2017-06-01 09:38:24 +0800

    27 done

--- 27-class.rb
+++ 27-class.rb
@@ -1,16 +1,18 @@
 class Person
-  # ...
+    attr_accessor :first_name
+    attr_accessor :last_name
+
+    def greet
+        puts "Hello, #{first_name} #{last_name}"
+    end
 end
 
 p1 = Person.new
-p1.first_name = "Peter"
-p1.last_name = "Wang"
+p1.first_name = 'Peter'
+p1.last_name = 'Wang'
 p1.greet # 输出 "Hello, Peter Wang"
 
 p2 = Person.new
-p2.first_name = "William"
-p2.last_name = "Zhang"
+p2.first_name = 'William'
+p2.last_name = 'Zhang'
 p2.greet # 输出 "Hello, William Zhang"
-
-
-
