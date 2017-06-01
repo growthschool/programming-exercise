94d69c25d7218f7743fd8f2c3e4222de751362ac
zhangyan <tongkusat123@gmail.com>
2017-06-01 09:33:30 +0800

    25 done

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -1,18 +1,23 @@
 # 计算一个阵列中各个元素的出现频率
-
+# 元素是key，频率是value
 def count(arr)
-  h = {}
+    h = {}
 
-  arr.each do |i|
-    # ...
-  end
+    arr.each do |i|
+        key = i.to_sym
 
-  return h # 回传一个 hash
+        h[key] = if c = h[key]
+                     c + 1
+                 else
+                     1
+                 end
+    end
+
+    h # 回传一个 hash
 end
 
-arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a", "c", "e", "a", "d", "e"]
+arr =  %w(a d d c b c c c d d e e e d a c e a d e)
 
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
