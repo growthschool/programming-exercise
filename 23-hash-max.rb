f7b51a7c2e5048d7a66b1d81a1a6417fc78beaef
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-02 00:12:34 +0800

    23-hash-max.rb

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -2,18 +2,19 @@
 
 def find_max(hash)
   # ...
+  hash.each do |key, value|
+    return key if value == hash.values.max
+  end
 end
 
 h = {
-  "a" => 71,
-  "b" => 38,
-  "c" => 21,
-  "d" => 80,
-  "e" => 10
+  'a' => 71,
+  'b' => 38,
+  'c' => 21,
+  'd' => 80,
+  'e' => 10
 }
 
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
