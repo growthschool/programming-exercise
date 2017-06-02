24dd478c40414a09cc47bbdfd0be9fb1aef8952e
megy <megy@megys-MacBook-Pro.local>
2017-06-02 22:46:03 +0800

    complete 13-nn.rb

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,6 +2,7 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
+n = n.to_i
 
 # while ( ... )
 #   while ( ...)
@@ -9,3 +10,12 @@ n = gets
 #   end
 # end
 
+i = 1
+while ( i <= n )
+  h = 1
+  while ( h <= n )
+    puts "#{i} * #{h}"
+    h += 1
+  end
+  i += 1
+end
