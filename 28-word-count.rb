58a8ea45257d300d6292f68b7444a2cbdcdc4245
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-02 09:15:31 +0800

    28-word-count.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,26 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
-doc = File.read("wordcount.txt")
+doc = File.read('wordcount.txt')
+def count(arr)
+  h = {}
+  brr = arr.uniq
+  brr.each do |j|
+    n = 0
+    arr.each do |i|
+      # ...
+      n += 1 if i == j
+    end
+    h[j] = n
+  end
 
+  h # 回传一个 hash
+end
 # ...
+length = 0
+arr = doc.split(' ')
+puts count(arr)
+arr.each do |i|
+  length += 1 if i.length == 1
+end
+
+puts "单字的个数为#{length}"
