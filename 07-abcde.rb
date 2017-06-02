98202c04bbe8d28a5f42f9484bb4c03de00a48f9
megy <megy@megys-MacBook-Pro.local>
2017-06-02 15:16:15 +0800

    complete 07-abcde.rb

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -10,13 +10,25 @@
 
 print "请输入一个整数x，然后按 Enter: "
 x = gets
+x = x.to_i
 
 print "请输入一个整数y，然后按 Enter: "
 y = gets
+y = y.to_i
 
 print "请输入一个整数z，然后按 Enter: "
 z = gets
+z = z.to_i
 
 # ....
+if x > 0
+  if y > 0
+    result = (z > 0) ? "B" : "C"
+  else
+    result = (z > 0) ? "D" : "E"
+  end
+else
+  result = "A"
+end
 
-puts "结果是________(A或B或C或D或E)"
+puts "结果是____#{result}____(A或B或C或D或E)"
