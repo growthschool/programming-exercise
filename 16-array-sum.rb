07299859b389b7dda58139e965132657aba9a73d
megy <megy@megys-MacBook-Pro.local>
2017-06-03 10:17:26 +0800

    complete 16-array-sum.rb

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,11 +1,16 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  #....
+  m = array[0]
+  for i in 1..((array.length)-1)
+    if array[i] > m
+      m = array[i]
+    end
+  end
+  m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
