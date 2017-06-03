3490b4d36e5d69b651b39f762aeffbcc5b427f5b
megy <megy@megys-MacBook-Pro.local>
2017-06-03 15:36:19 +0800

    complete 24-hash-even.rb

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -1,9 +1,16 @@
 # 给定一 Hash，输出 value 是偶数的 keys
 
 def find_even_keys(hash)
-
-  # ... (请回传一个数组)
-
+  result1 = []
+  h = hash.values
+  for i in h
+    result1 << i if i % 2 == 0
+  end
+  result2 = []
+  for j in result1
+    result2 << hash::key(j)
+  end
+  result2
 end
 
 h = {
@@ -17,5 +24,3 @@ h = {
 answer = find_even_keys(h)
 
 puts "有偶数 value 的 keys 有 #{answer}" # 应该是数组 [b,d,e]
-
-
