4fdf9cf7c44a5d60dcc8271785f1eef047383176
megy <megy@megys-MacBook-Pro.local>
2017-06-03 15:17:57 +0800

    complete 22-missing.rb

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,12 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  arr.uniq
+  result = []
+  for i in 0..9
+    result << i if !arr.include?(i)
+  end
+  result
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
