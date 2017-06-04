7c08b4be3866c89f2c45e02636abac2935228606
shaheming <hemingsha@gmail.com>
2017-06-04 15:58:36 +0800

    03+

--- 03-triangle.rb
+++ 03-triangle.rb
@@ -3,8 +3,18 @@
 print "请输入直角三角形的高，然后按 Enter: "
 a = gets
 
+while(!(a =~/^\d+$/ or a =~/^\d+\.\d+$/))
+  puts "请输入正确的数字"
+  a = gets
+end
+
 print "请输入直角三角形的底边，然后按 Enter: "
 b = gets
+
+while(!(b =~/^\d+$/ or b =~/^\d+\.\d+$/))
+  puts "请输入正确的数字"
+  b = gets
+end
 
 area = a.to_f * b.to_f / 2
 # .....
