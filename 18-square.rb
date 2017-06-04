6e46e91f83fa148d38f08be29936365217191087
shaheming <hemingsha@gmail.com>
2017-06-04 19:00:34 +0800

    18

--- 18-square.rb
+++ 18-square.rb
@@ -2,8 +2,22 @@
 
 arr = []
 
+pattern = /^\d+$/
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets.chomp
+
+while !pattern.match(n)
+  print "请输入数字 N，然后按 Enter: "
+  n = gets.chomp
+end
+
+N = pattern.match(n)[0].to_i
+puts N
+i = 0 
+while (i < N )
+  arr << i**2
+  i+=1
+end
 
 # ...
 
