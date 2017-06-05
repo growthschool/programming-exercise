7f0cb7845758078e1bd7ddfa0fb692efae539b4b
shaheming <hemingsha@gmail.com>
2017-06-05 22:42:54 +0800

    28

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,15 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
 doc = File.read("wordcount.txt")
+arr = doc.split(/\W+/)
+dic = {}
+arr.each do |w|
+ if dic[w.downcase]
+   dic[w.downcase] += 1
+ else
+  dic[w.downcase] = 1
+ end
+end
 
+puts dic.to_a.sort_by{|k,v| -v }.to_s
 # ...
