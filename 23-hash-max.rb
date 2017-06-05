f518ccd56ef7ea83952e4377b92b82ab64868381
shaheming <hemingsha@gmail.com>
2017-06-05 21:42:27 +0800

    23

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -2,6 +2,15 @@
 
 def find_max(hash)
   # ...
+  max = 0
+  maxKey = ""
+  hash.each do |key,value|
+    if value > max
+      max = value
+      maxKey = key.to_s
+    end
+  end
+  return maxKey
 end
 
 h = {
@@ -15,5 +24,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
