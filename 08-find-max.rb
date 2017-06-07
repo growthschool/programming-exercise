556b997cde92a62e2695c76cf4b73794aac27d99
yaoyao <yao_zhou@icloud.com>
2017-06-07 01:19:02 -0700

    08 done

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -9,6 +9,24 @@ y = gets
 print "请输入一个数字z，然后按 Enter: "
 z = gets
 
-# ....
+x = x.to_f
+y = y.to_f
+z = z.to_f
 
-puts "最大的数是 ________(x或y或z)"
+if x < y
+  if y > z
+    puts "最大的数是y"
+  else
+    puts "最大的数是z"
+  end
+else
+  if y > z
+    puts "最大的数是x"
+  else
+    if x > z
+      puts "最大的数是x"
+    else
+      puts "最大的数是z"
+    end
+  end
+end
