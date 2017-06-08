121d5ae4c0e97a70143030f0648a857a7c774285
qinfeng <q884822@gmail.com>
2017-06-08 22:50:49 +0800

    014

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,13 +1,21 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  if n > 2 && n % 2 == 0
+    return false
+  else
+    return true
+  end
+
 end
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
+
 if is_prime(n.to_i)
+
+
   puts "这是质数"
 else
   puts "这不是质数"
