3b0665078509e8f0b07cc31e384e5acf62021349
qinfeng <q884822@gmail.com>
2017-06-10 01:31:41 +0800

    选择排序

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,15 +1,7 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
-def select_sort(array)
-  return array if array.length == 0
-  (0...array.length).each do |i|
-    min, index = array[i], i
-    (i...array.length).each { |j| min, index = array[j], j if array[j] < min }
-    array[i], array[index] = array[index] , array[i]
-  end
-  array
-end
+
 
 
 def selection_sort(arr)
@@ -17,11 +9,7 @@ def selection_sort(arr)
 
   (0...arr.size).each do |i| # 從 0 開始走訪數組
     (i...arr.size).each do |j| # 從 i 開始走訪數組
-      puts arr[i]
-      puts arr[j]
       if arr[i] > arr[j] # 如果 arr[j] 比較大，就交換 arr[i] 跟 arr[j]
-
-
         tmp = arr[i]
         arr[i] = arr[j]
         arr[j] = tmp
--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,15 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  return arr if arr.size == 0
+  arr2 = [0,1,2,3,4,5,6,7,8,9]
+
+  (0...arr.size).each do |i|
+
+ 
+  end
+
+
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
