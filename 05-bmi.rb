c2ebbb2cc3ddcbff423cda8139d0dea075e0213e
klpk011 <klpk011@klpk011.local>
2017-06-12 17:19:49 +0800

    05-bmi

--- 05-bmi.rb
+++ 05-bmi.rb
@@ -10,8 +10,19 @@ weight = gets
 print "请输入您的身高(厘米)，然后按 Enter: "
 height = gets
 
-# .....
+a = weight.to_f
+b = height.to_f/100
 
-puts "您的 BMI 是: _________"
+bim = a/(b*b)
 
-puts "您的 BMI 结果是: _________(过轻或正常或过重)"
+if bim < 18.5
+  c = "过轻"
+elsif bim >=24
+  c = "过重"
+else bim > 18.5 && bim < 24
+  c = "正常"
+end
+
+puts "您的 BMI 是: #{bim}"
+
+puts "您的 BMI 结果是: #{c}"
