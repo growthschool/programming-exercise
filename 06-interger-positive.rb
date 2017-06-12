7016c472a6d8bcdeacc3d025eeee97e663fbc17c
klpk011 <klpk011@klpk011.local>
2017-06-12 17:39:02 +0800

    06-interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -4,7 +4,22 @@
 print "请输入一个整数，然后按 Enter: "
 x = gets
 
+
+if x.to_i > 0
+  Y = "正数"
+elsif x.to_i == 0
+  Y = "零"
+else
+  Y = "负数"
+end
+
+if x.to_i%2 != 0
+  Z = "奇数"
+else
+  Z = "偶数"
+end
+
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+puts "这个数是#{Y}"
+puts "这个数是#{Z}"
