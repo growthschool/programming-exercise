f7d8443aece24439f756dae5da4aacebedb93e80
qinfeng <q884822@gmail.com>
2017-06-13 18:24:02 +0800

    026

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,9 +8,33 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
-# ....
+# 去掉小于16
+arr.each do |a|
+  a.each do |key, value|
+    if key == "age"
+      if value < 16
+        arr.delete(a)
+      end
+    end
+  end
+end
 
-puts "所有成年人，并由小到大: _________"
+# 排序
+(0...arr.size).each do |i| # 從 0 開始走訪數組
+  (i...arr.size).each do |j| # 從 i 開始走訪數組
+    if arr[i].fetch("age") > arr[j].fetch("age") # 如果 arr[j] 比較大，就交換 arr[i] 跟 arr[j]
+      tmp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = tmp
+    end
+
+  end
+end
+
+
+
+
+puts "所有成年人，并由小到大: __#{arr}_______"
 
 # 答案应该是
 #[
