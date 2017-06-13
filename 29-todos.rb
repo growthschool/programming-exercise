fde6a99912c6b87364488f9477f0edee3d495747
zhangyan <tongkusat123@gmail.com>
2017-06-13 18:27:51 +0800

    29 partially done

--- 29-todos.rb
+++ 29-todos.rb
@@ -1,33 +1,54 @@
 # 简易 Todo 代办事项应用
+def add_todo(todos, t)
+    todos.push(t)
+end
 
-text = File.read("todos.txt")
+def remove_todo(todos, i)
+    todos.delete_at(i)
+end
+
+def save_todo(todos)
+    # File.truncate('todos.txt', 0)
+    File.open('todos.txt', 'w') do |f|
+        todos.each do |i|
+            f << i.to_s
+            f << "\n"
+        end
+    end
+end
+
+text = File.read('todos.txt')
 
 todos = []
 text.each_line do |line|
-  todos << line.chomp
+    todos << line.chomp
 end
 
 todos.each_with_index do |todo, index|
-  puts "#{index}: #{todo}"
+    puts "#{index}: #{todo}"
 end
 
-while (true)
-  print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
-  command = gets.chomp
+loop do
+    print '请输入指令 1. add 2. remove 3. save，然后按 Enter: '
+    command = gets.chomp
 
-  if command == "add"
-    print "请输入代办事项: "
-    # ...
-  elsif command == "remove"
-    print "请输入要删除的编号: "
-    # ...
-  elsif command == "save"
-    puts "存盘离开"
+    if command == 'add'
+        print '请输入代办事项: '
 
-    # ...
-    break;
-  else
-    puts "看不懂，请再输入一次"
-  end
+        thing = gets
+        add_todo(todos, thing)
+    elsif command == 'remove'
+        print '请输入要删除的编号: '
+
+        index = gets
+        remove_todo(todos, index.to_i)
+    elsif command == 'save'
+        puts '存盘离开'
+
+        save_todo(todos)
+        # ...
+        break
+    else
+        puts '看不懂，请再输入一次'
+    end
 end
-
