e86dec67498d131b9d36bb8b0c660bbe3bae9d76
yaoyao <yao_zhou@icloud.com>
2017-06-13 21:24:52 -0700

    17 done

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -14,7 +14,9 @@ end
 
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+
+
+puts "总和是 #{arr.reduce(&:+)}"
+puts "平均是 #{arr.reduce(&:+).to_f / arr.length }"
+puts "最大值是 #{arr.max}"
+puts "最小值是 #{arr.min}"
