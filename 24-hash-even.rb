4156b9a83a3075dcca35efc4f5c8d6cc8a312d30
qinfeng <q884822@gmail.com>
2017-06-15 01:51:28 +0800

    24 fix

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -3,15 +3,26 @@
 def find_even_keys(hash)
 
   arr = []
-  (0...hash.values.size).each do |i|
-
-     if hash.values[i] % 2 == 0
-       puts hash.key(hash.values[i])
-       arr.push(hash.key(hash.values[i]))
-     end
-
+  # (0...hash.values.size).each do |i|
+  #
+  #    if hash.values[i] % 2 == 0
+  #      puts hash.key(hash.values[i])
+  #      arr.push(hash.key(hash.values[i]))
+  #    end
+  #
+  # end
+  hash.values.each do |i|
+    if i % 2 == 0
+      arr << i
+    end
   end
-  return arr
+  arr2 = []
+
+  arr.each do |y|
+    arr2 << hash.key(y)
+  end
+
+  return arr2
 
 end
 
