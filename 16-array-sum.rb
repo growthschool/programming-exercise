f9ea13b9b6f7908dfe1875185ec8976e2f124c95
learnor <learnor@gmail.com>
2017-06-14 13:49:47 +0800

    16 finished

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -2,10 +2,17 @@
 
 def find_max(array)
   #....
+  m = array[0]
+  for i in (1..array.length-1) do
+    if m < array[i]
+      m = array[i]
+    end
+    i += 1
+  end
+  m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
