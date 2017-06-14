17384d6da54820c6fbf6ae95bedbc9ed959eb534
panxiubin <xiubinpan@gmail.com>
2017-06-14 15:59:29 +0800

    14-prime

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,7 +1,13 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  for i in 2..n/2
+    if n % i == 0
+      return false
+    else
+      return true
+    end
+  end
 end
 
 print "请输入数字 N，然后按 Enter: "
@@ -12,3 +18,8 @@ if is_prime(n.to_i)
 else
   puts "这不是质数"
 end
+
+
+# 素数，又称质数，是只能被1或者自己整除的自然数。
+# 比1大但不是素数的数我们称之为合数，1和0即非素数也非合数。
+# 素数的属性称为素性，素数在数论中处于基本的重要地位。
