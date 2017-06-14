f170543f61ff9ad22d32edcaa65ab48ba5438e17
panxiubin <xiubinpan@gmail.com>
2017-06-14 17:50:53 +0800

    23-hash-max

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -1,7 +1,9 @@
 # 给定一 Hash，输出有最大 value 的 key
 
 def find_max(hash)
-  # ...
+  arr = hash.values
+  max = arr.sort.last
+  return hash.key(max)
 end
 
 h = {
@@ -15,5 +17,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
