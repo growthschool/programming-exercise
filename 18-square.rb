6fb77f626f632d3a1ebfb76d2b8de989e9a59e3b
Caven <caven@CavendeMacBook-Pro.local>
2017-06-18 16:46:43 +0800

    ex18.rb

--- 18-square.rb
+++ 18-square.rb
@@ -3,7 +3,18 @@
 arr = []
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets.to_i
+
+another = []
+
+(1..n).each do |num|
+  another.push(num)
+end
+
+another.each_with_index do |number, index|
+  arr.push(index * index)
+end
+
 
 # ...
 
