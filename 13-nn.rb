8300defad64879d0a770fff4cd1941207e609512
klpk011 <klpk011@klpk011.local>
2017-06-20 11:55:37 +0800

    13-nn.rb

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,10 +2,20 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
-
+m = n.to_i
+x = 0
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
 
+while (x <= m)
+  y = 0
+  while (y <= x)
+  print x,"x",y, "=",x*y,"  "
+   y +=1
+ end
+print "\n"
+ x +=1
+ end
