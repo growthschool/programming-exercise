557644393a13cc7cc8558b7cf66797c6a1e00182
klpk011 <klpk011@klpk011.local>
2017-06-23 09:34:24 +0800

    27-class

--- 27-class.rb
+++ 27-class.rb
@@ -1,9 +1,13 @@
 class Person
-  # ...
+  attr_accessor :first_name, :last_name
+  def greet
+   puts "Hello,#{@first_name} #{@last_name}"
+  end
 end
 
+
+
 p1 = Person.new
-p1.first_name = "Peter"
 p1.last_name = "Wang"
 p1.greet # 输出 "Hello, Peter Wang"
 
@@ -11,6 +15,3 @@ p2 = Person.new
 p2.first_name = "William"
 p2.last_name = "Zhang"
 p2.greet # 输出 "Hello, William Zhang"
-
-
-
