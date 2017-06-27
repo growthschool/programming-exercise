314671ea22468e584303211bffb976cd5e75838a
dongmy54 <615760433@qq.com>
2017-06-27 19:55:46 +0800

    complete 26-hash-filter

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,9 +8,38 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
+def hash_filter(arr)
+	arr.each_with_index do |a,i|
+  	 if a["age"] < 18
+     	arr.delete_at(i)
+  	 end
+	end
+ 
+  l = arr.size
+  k = 1
+  
+  while (k <= l)
+    j = k
+   
+    while (arr[j] != nil)
+   		if arr[j]["age"] < arr[k-1]["age"]
+   			temp = arr[k-1]
+   			arr[k-1] = arr[j]
+   			arr[j] = temp
+   		end
+   		j += 1
+    end
+   
+   k += 1	
+  end
+
+ return arr.to_s
+end
+#end
+
 # ....
 
-puts "所有成年人，并由小到大: _________"
+puts "所有成年人，并由小到大: #{hash_filter(arr)}"
 
 # 答案应该是
 #[
