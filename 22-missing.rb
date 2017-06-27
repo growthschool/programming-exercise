a9e5d30b8f0b3129e7b164858e14cd1e76f69020
dongmy54 <615760433@qq.com>
2017-06-27 15:27:53 +0800

    complete 22-missing

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,14 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  miss_arr = []
+  
+  for i in 0..9
+    if !arr.include?(i)
+       miss_arr << i
+    end
+  end
+  return miss_arr
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
