92a94eb2a97d3f9627de2024ca9f164364c0697d
like <20783506632@qq.com>
2017-06-28 19:49:55 +0800

    sum event

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,19 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
 
+while ( n <= 12 )
+    puts n
+    n = n + 0
+  while ( n <= 12)
+   puts n
+    n = n + 1
+    n = n * n
+  end
+end
+
+# x = 10
+# while x <= 10
+#   puts x
+#   x = x + 1
+# end
