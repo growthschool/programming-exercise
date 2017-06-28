ada68d7234c46daa27d53dae03dae29e415f2c21
Carrie <linger1834@163.com>
2017-06-28 14:41:48 +0800

    question 08

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -9,6 +9,18 @@ y = gets
 print "请输入一个数字z，然后按 Enter: "
 z = gets
 
-# ....
+x = x.to_f
+y = y.to_f
+z = z.to_f
 
-puts "最大的数是 ________(x或y或z)"
+if x > y && x > z
+  result = "x"
+elsif y > x && y > z
+  result = "y"
+elsif z > x && z > y
+  result = "z"
+else
+  result = "：错误，有两个数一样大，请重新输入！"
+end
+
+puts "最大的数是#{result}"
