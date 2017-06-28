4a7f81bfece635d4e1ff3bdf0e307236ab965cf7
like <20783506632@qq.com>
2017-06-28 11:37:28 +0800

    implement loop seven

--- 11-seven.rb
+++ 11-seven.rb
@@ -1,9 +1,15 @@
 # 题目: 列出 1 到 100 之间，所有 7 的倍数
 
-i = 1
-while ( i <= 100 )
-
-puts i
-i = i*7
+i = 7
+while i <= 100
+    puts i
+    # i = i*7
+    i = i + 7
   # i+=1
 end
+
+# x = 1
+# while x <= 10
+#   puts x
+#   x = x + 1
+# end
--- 12-sum-even.rb
+++ 12-sum-even.rb
@@ -3,9 +3,7 @@
 i = 1
 total = 0
 
-while ( i <= 100 )
-
-  # ....
+while  i <= 100
 
   i+=1
 end
