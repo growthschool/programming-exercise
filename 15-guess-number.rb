05bb5d805265869f3799ea36453e38a4c590d836
Carrie <linger1834@163.com>
2017-06-28 15:47:21 +0800

    question 15

--- 15-guess-number.rb
+++ 15-guess-number.rb
@@ -5,9 +5,13 @@ target = rand(100)
 while (true)
   print "请猜一个 0~99 的数字 N，然后按 Enter: "
   n = gets
-
-  #puts "太低了，再猜一次"
-  #puts "太高了，再猜一次"
+  n = n.to_i
+  
+  if n < target
+    puts "太低了，再猜一次"
+  else n > target
+    puts "太高了，再猜一次"
+  end
 
   if n.to_i == target
     puts "恭喜猜中啦! "
