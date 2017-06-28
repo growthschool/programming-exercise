b20931af64552e63490590b523bd3debeb0cc85a
梁信众 <johnson.liangxinzhong@gmail.com>
2017-06-28 22:39:12 +0800

    14-prime

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,8 +1,25 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  res = [1]
+  res << n
+  if n == 0 || n == 1
+    return false
+  end
+  2.upto(10) do |x|
+    #如果有自己的话,就跳下一次循环
+    if n == x
+      next
+    end
+    #看看是否能被 2-10之间的数整除, 取余数相当于分组
+    if n % x == 0
+      res << x
+    end
+  end
+  res.length > 2 ? false : true
 end
+
+
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
