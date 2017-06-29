271e32a8aee569249549e35b8728db11b5135a4a
Antarts <m.dream@foxmail.com>
2017-06-29 17:14:37 +0800

    07-abcde

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -17,6 +17,20 @@ y = gets
 print "请输入一个整数z，然后按 Enter: "
 z = gets
 
-# ....
+x = x.to_i
+y = y.to_i
+z = z.to_i
 
-puts "结果是________(A或B或C或D或E)"
+if x > 0 && y > 0 && z > 0
+  p = "B"
+elsif x > 0 && y > 0 && z < 0
+  p = "C"
+elsif x > 0 && y < 0 && z > 0
+  p = "D"
+elsif x > 0 && y < 0 && z < 0
+  p = "E"
+elsif x < 0
+  p = "A"
+end
+
+puts "结果是____#{p}____"
