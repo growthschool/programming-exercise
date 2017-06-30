a7159811ff1059d8a4688e903dc8ea04f31e4d2d
Antarts <m.dream@foxmail.com>
2017-07-01 00:18:37 +0800

    array-stats-avg

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -14,6 +14,13 @@ def find_min(array)
   array.min
 end
 
+def avg(array)
+  sum = array.inject(0) { |s, i| s + i }
+  avg = sum / array.length
+  return avg if array.length * avg == sum
+  sum * 1.0 / array.length
+end
+
 while (true)
   print "请输入数字，结束请直接按 Enter: "
   user_input = gets
@@ -24,6 +31,7 @@ while (true)
   end
 end
 
+avg = avg(arr)
 sum = get_sum(arr)
 max = find_max(arr)
 min = find_min(arr)
@@ -31,6 +39,6 @@ min = find_min(arr)
 puts arr.to_s
 
 puts "总和是 __#{sum}___"
-puts "平均是 _____"
+puts "平均是 __#{avg}___"
 puts "最大值是 __#{max}___"
 puts "最小值是 __#{min}___"
