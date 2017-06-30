14e99663aabde9dc178e6a1a3bc8fec918d0f00b
Antarts <m.dream@foxmail.com>
2017-06-30 23:58:55 +0800

    17-array-stats

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -2,6 +2,18 @@
 
 arr = []
 
+def get_sum(array)
+  array.inject(0){ |sum,x| sum + x }
+end
+
+def find_max(array)
+  array.max
+end
+
+def find_min(array)
+  array.min
+end
+
 while (true)
   print "请输入数字，结束请直接按 Enter: "
   user_input = gets
@@ -12,9 +24,13 @@ while (true)
   end
 end
 
+sum = get_sum(arr)
+max = find_max(arr)
+min = find_min(arr)
+
 puts arr.to_s
 
-puts "总和是 _____"
+puts "总和是 __#{sum}___"
 puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts "最大值是 __#{max}___"
+puts "最小值是 __#{min}___"
