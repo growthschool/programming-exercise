bb42594dfb18b1adbec218b67a7c1a5b5e52d52f
梁信众 <johnson.liangxinzhong@gmail.com>
2017-06-30 19:12:43 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,14 @@
 
 doc = File.read("wordcount.txt")
 
+  list = Hash.new(0)
+
+  doc.downcase.scan(/\w+/) {|w| list[w] += 1}
+
+  list
+
+
+
+puts list
+puts "Total words: #{doc.scan(/\w+/).count}"
 # ...
