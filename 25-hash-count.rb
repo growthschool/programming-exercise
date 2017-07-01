4972c5e53c3cfc8fcbcb58dfafd65a20cf2e1f9c
yangyepeng <yangyepeng@gmail.com>
2017-07-01 22:02:49 +0800

    25-hash-count.rb

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -2,9 +2,14 @@
 
 def count(arr)
   h = {}
-
   arr.each do |i|
-    # ...
+    num = 0
+    arr.each do |e|
+      if i == e
+         num = num + 1
+       end
+    end
+    h[i] = num
   end
 
   return h # 回传一个 hash
@@ -16,3 +21,4 @@ answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
 
+# num = 0 需要写在第二个循环之外，因为每一次新的循环，num需要清零
