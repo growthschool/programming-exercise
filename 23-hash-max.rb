dda49a2425e5e0f4725a92d9ac2a34d163ca9780
yangyepeng <yangyepeng@gmail.com>
2017-07-01 21:32:25 +0800

    23-hash-max.rb

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -1,7 +1,8 @@
 # 给定一 Hash，输出有最大 value 的 key
 
 def find_max(hash)
-  # ...
+  hash.max_by{|k,v| v}
+
 end
 
 h = {
@@ -16,4 +17,5 @@ answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
 
-
+# 第二种解法
+# hash.select {|k,v| v == hash.values.max }
