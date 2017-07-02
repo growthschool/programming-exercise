fd14060fce599589b94e7ae70c287cc83064ba42
Antarts <m.dream@foxmail.com>
2017-07-02 21:54:15 +0800

    18-square

--- 18-square.rb
+++ 18-square.rb
@@ -1,10 +1,13 @@
 # 建构一个阵列有一百个的元素，内容是 0, 1, 4, 9, 16, 25...... 每个元素是该索引的平方
 
 arr = []
-
+def array_squ(a)
+  a -= 1
+  (0..a).map { |a| a**2 }
+end
 print "请输入数字 N，然后按 Enter: "
 n = gets
-
-# ...
+n = n.to_i
+arr = array_squ(n)
 
 puts arr.to_s
