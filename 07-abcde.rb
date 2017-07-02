36b38684fecf74ac56842e536cbf18af8b08a488
huangpuxiang <hpx0809@outlook.com>
2017-07-02 16:49:17 +0800

    08-find-max

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -8,15 +8,35 @@
 #     当 z > 0 输出 "D"
 #     当 z < 0 输出 "E"
 
+
+
+
 print "请输入一个整数x，然后按 Enter: "
 x = gets
+i = x.to_i
 
 print "请输入一个整数y，然后按 Enter: "
 y = gets
+j = y.to_i
 
 print "请输入一个整数z，然后按 Enter: "
 z = gets
+k = z.to_i
 
-# ....
-
-puts "结果是________(A或B或C或D或E)"
+if i < 0
+  puts "A"
+else
+  if j > 0
+    if k > 0
+      puts "B"
+    else
+      puts "C"
+    end
+  else
+    if k > 0
+      puts "D"
+    else
+      puts "E"
+    end
+  end
+end
--- 08-find-max.rb
+++ 08-find-max.rb
@@ -2,13 +2,26 @@
 
 print "请输入一个数字x，然后按 Enter: "
 x = gets
+x1 = x.to_i
 
 print "请输入一个数字y，然后按 Enter: "
 y = gets
+y1= y.to_i
 
 print "请输入一个数字z，然后按 Enter: "
 z = gets
+z1 = z.to_i
 
-# ....
+if x1 >= y1
+  a = x1
+else
+  a = y1
+end
 
-puts "最大的数是 ________(x或y或z)"
+ if a >= z1
+   b = a
+ else
+   b = z1
+ end
+
+puts "最大的数是#{b}"
