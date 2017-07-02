78850d5614887468d4d35b58b8658371281a31eb
Antarts <m.dream@foxmail.com>
2017-07-02 23:40:07 +0800

    23-hash-max.rb

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -1,7 +1,7 @@
 # 给定一 Hash，输出有最大 value 的 key
 
 def find_max(hash)
-  # ...
+  hash.max_by { |k, v| v }
 end
 
 h = {
@@ -15,5 +15,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
