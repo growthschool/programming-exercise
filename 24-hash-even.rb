551b882f9c3e02e3e389cfa6d2bb7397e0e7a843
Antarts <m.dream@foxmail.com>
2017-07-02 23:53:46 +0800

    24-hash-even

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -2,7 +2,7 @@
 
 def find_even_keys(hash)
 
-  # ... (请回传一个数组)
+  hash.select { |k, v| v.even? }.keys
 
 end
 
@@ -17,5 +17,3 @@ h = {
 answer = find_even_keys(h)
 
 puts "有偶数 value 的 keys 有 #{answer}" # 应该是数组 [b,d,e]
-
-
