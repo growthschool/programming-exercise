bf7b378c3289e8c6351069d3f8450a60b7f15bab
yangyepeng <yangyepeng@gmail.com>
2017-07-02 10:48:46 +0800

    29-todos.rb

--- 29-todos.rb
+++ 29-todos.rb
@@ -11,23 +11,38 @@ todos.each_with_index do |todo, index|
   puts "#{index}: #{todo}"
 end
 
+def show(arr)
+  arr.each_with_index do |todo, index|
+    puts "#{index}: #{todo}"
+  end
+ end
+
 while (true)
   print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
   command = gets.chomp
 
   if command == "add"
     print "请输入代办事项: "
-    # ...
+    todos << gets.chomp
+    show(todos)
+
   elsif command == "remove"
     print "请输入要删除的编号: "
-    # ...
+    n = gets.chomp.to_i
+    todos.delete_at(n)
+    show(todos)
+
   elsif command == "save"
     puts "存盘离开"
 
-    # ...
+    File.open("todos.txt", "w+") do |f|
+      todos.each do |t|
+        f << t + "\n"
+      end
+    end
+
     break;
   else
     puts "看不懂，请再输入一次"
   end
 end
-
