01b3491e519837e4aaff0504243d9192db14b323
yangyepeng <yangyepeng@gmail.com>
2017-07-02 10:40:23 +0800

    28-word-count.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,15 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+frequency = Hash.new(0)
+doc.downcase.scan(/\b[a-z]{3,16}\b/) {|word| frequency[word] = frequency[word] + 1}
+frequency.keys.sort.each{|key| print key,' => ',frequency[key], "\n"}
+
+
+
+# frequency = Hash.new(0)
+# input_file = File.open('omim.txt', 'r')
+# output_file = File.open('wordfrequency.txt', 'w')
+# input_file.read.downcase.scan(/\b[a-z]{3,16}\b/) {|word| frequency[word] = frequency[word] + 1}
+# frequency.keys.sort.each{|key| output_file.print key,' => ',frequency[key], "\n"}
+# exit
