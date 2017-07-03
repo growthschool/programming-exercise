df24763ae280bd65e6ee37b6f73f2e6e794d3d2c
liubingjie <breeder1121@hotmail.com>
2017-07-03 18:32:54 +0800

    ch08

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -1,14 +1,23 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
 print "请输入一个数字x，然后按 Enter: "
-x = gets
+x = gets.to_i
 
 print "请输入一个数字y，然后按 Enter: "
 y = gets
+y = y.to_i
 
 print "请输入一个数字z，然后按 Enter: "
 z = gets
 
-# ....
+def max(x,y,z)
+  if x > y && x > z
+    x
+  elsif y > z
+    y
+  else
+    z
+  end
+end
 
-puts "最大的数是 ________(x或y或z)"
+puts "最大的数是 #{max(x, y, z.to_i)}"
