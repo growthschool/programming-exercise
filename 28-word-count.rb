5e2748259af1346bd23f0f55ed9489d157cc6391
Antarts <m.dream@foxmail.com>
2017-07-03 21:52:09 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,27 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+wordcount = {}   # link: https://gist.github.com/etagwerker/726979
+
+file = doc
+
+puts "Indexing #{doc}"
+
+file.each_line do |line|
+  words = line.split
+  words.each do |word|
+    word = word.gsub(/[,.()'"]/,'')
+    if wordcount[word]
+      wordcount[word] += 1
+    else
+      wordcount[word] = 1
+    end
+  end
+end
+
+
+puts "Words count:"
+
+wordcount.sort {|a,b| a[1] <=> b[1]}.each do |key, value|
+  puts "#{key} => #{value}"
+end
