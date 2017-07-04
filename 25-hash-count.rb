08ce05f17306926dfaba31c861a687acd0499d6c
wangxing <18912540921@163.com>
2017-07-04 21:35:44 +0800

    Q25

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -2,9 +2,20 @@
 
 def count(arr)
   h = {}
+  i = 0
+  n = arr.size
 
-  arr.each do |i|
-    # ...
+  while (i < n)
+    a = arr.first
+    value = 0
+    arr.each do |i|
+      if a == i
+        value +=1
+      end
+      h[i] = value
+    end
+    arr = arr - [a]
+    i += 1
   end
 
   return h # 回传一个 hash
@@ -15,4 +26,3 @@ arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
