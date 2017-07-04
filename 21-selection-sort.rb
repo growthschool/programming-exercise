0d54c2c9b3e50dcabdd8d2bb83319d725c6a1c1d
liubingjie <breeder1121@hotmail.com>
2017-07-04 17:18:08 +0800

    ch21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -3,6 +3,19 @@
 
 def insertion_sort(arr)
   #...
+  i = 0
+  while i < ( arr.size - 1 )
+    j = i + 1
+    while j < arr.size
+      if arr[i] > arr[j]
+        arr[i], arr[j] = arr[j], arr[i]
+      end
+      j += 1
+    end
+
+    i += 1
+  end
+  arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
