b979ebcbd680b2f28b9a55cc6a86312e35d5b862
SegueX <free1989xj@hotmail.com>
2017-07-04 23:06:59 +0800

    integer-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -4,7 +4,34 @@
 print "请输入一个整数，然后按 Enter: "
 x = gets
 
-# ....
+odd = "奇数"
+even = "偶数"
+positive = "正数"
+negative = "负数"
+zero = "零"
+x = x.to_i
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if x % 2 == 0
+  if x > 0
+    x1 = even
+    x2 = positive
+  elsif x < 0
+    x1 = even
+    x2 = negative
+  else
+    x1 = even
+    x2 = zero
+  end
+else
+  if x > 0
+    x1 = odd
+    x2 = positive
+  else
+    x1 = odd
+    x2 = negative
+  end
+
+end
+
+puts "这个数是__#{x2}___ (正数或零或负数)"
+puts "这个数是__#{x1}___ (偶数或奇数)"
