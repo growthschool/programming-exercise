f62dee2de13ad1979f963a76ef2675b662081335
liubingjie <breeder1121@hotmail.com>
2017-07-04 15:32:06 +0800

    ch13

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,10 +2,19 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
+n = n.to_i
+i = 0
+ while ( i <= n )
+   j = 0
+   while ( j <= n )
+     puts " #{i} x #{j} = #{i * j}"
+     j += 1
+   end
+   i += 1
+ end
 
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
