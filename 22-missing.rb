667bf44d9961c7512d653e6e6d100ab04d70db9f
wangxing <18912540921@163.com>
2017-07-04 20:55:36 +0800

    Q22

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,13 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  b = []
+  i = 0
+  while (i < 10)
+    b << i
+    i += 1
+  end
+  c = b - arr.uniq
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
