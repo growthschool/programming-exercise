0f02d9cd977656f71ebf2f6a38d24df4196f0b32
echo <zhouhx03411@gmail.com>
2017-07-05 09:14:27 +0800

    finished 06-exercise

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -4,7 +4,23 @@
 print "请输入一个整数，然后按 Enter: "
 x = gets
 
+if x.to_i >0
+  y = "正数"
+elsif x.to_i<0
+  y = "负数"
+elsif x.to_i == 0
+  y= "零"
+end
+
+z = x.to_i - (x.to_i/2)*2
+if z == 0
+  a = "偶数"
+else
+  a = "奇数"
+end
+
+
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+puts "这个数是__#{y}___ (正数或零或负数)"
+puts "这个数是___#{a}__ (偶数或奇数)"
