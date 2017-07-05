d77a27b980bdbfba001a48346ebf86cde11fe5cb
echo <zhouhx03411@gmail.com>
2017-07-05 12:49:19 +0800

    finished 22-missing

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,6 +1,25 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
+  i = 0
+  arr_miss =[]
+  j= 0
+
+  while i<10
+
+    arr.each_with_index do |array,index|
+      if i == array
+       break
+      end
+      j = index+1
+    end
+    if j == arr.size
+      arr_miss.push(i)
+    end
+    i+= 1
+  end
+
+  return arr_miss
   # ...
 end
 
