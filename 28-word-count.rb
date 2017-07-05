5b110c61968d8203359b615c002496509d6a2073
echo <zhouhx03411@gmail.com>
2017-07-05 15:05:28 +0800

    finished 28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,14 @@
-# 请打开 wordcount.txt，计算每个单字出现的次数
+# 请打开 wordcount.txt，计算每个单字出现的次
 
+doc = ""
+array = []
+# file = File.open("wordcount.txt")
+# doc = file.read("wordcount.txt")
 doc = File.read("wordcount.txt")
+array = doc.split("\n")
+
+puts doc
+
+puts array
 
 # ...
