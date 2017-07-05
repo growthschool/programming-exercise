b4a2057b5d62937fbd1ee3e0ec943320624c7f7d
echo <zhouhx03411@gmail.com>
2017-07-05 10:04:36 +0800

    finished 10-function

--- 10-function.rb
+++ 10-function.rb
@@ -1,6 +1,16 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
 def find_max(x, y, z)
+  if x.to_i > y.to_i
+    max = x
+  elsif x.to_i <y.to_i
+    max = y
+  end
+
+  if max.to_i < z.to_i
+     max = z
+  end
+  return max
 end
 
 print "请输入一个数字x，然后按 Enter: "
