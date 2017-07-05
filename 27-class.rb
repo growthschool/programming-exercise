a0ea6f9e445b8d90bac0f52e9d2496839efffcef
wangxing <18912540921@163.com>
2017-07-05 09:19:16 +0800

    Q27

--- 27-class.rb
+++ 27-class.rb
@@ -1,6 +1,13 @@
 class Person
-  # ...
+  attr_accessor :first_name
+  attr_accessor :last_name
+
+  def greet
+    puts "Hello, #{@first_name} #{@last_name}"  #是否有@输出结果都一致
+  end
 end
+
+
 
 p1 = Person.new
 p1.first_name = "Peter"
@@ -11,6 +18,3 @@ p2 = Person.new
 p2.first_name = "William"
 p2.last_name = "Zhang"
 p2.greet # 输出 "Hello, William Zhang"
-
-
-
