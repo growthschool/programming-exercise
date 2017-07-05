94800ba86ba3cba0b5188b6bb991b45008260e9c
echo <zhouhx03411@gmail.com>
2017-07-05 10:29:59 +0800

    finished 13-nn

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,25 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
+i = 2
+max = n.to_i/2
+
+while i<= max
+   if n.to_i % i == 0
+     puts "n 不是质数"
+     break
+   end
+
+   if i == max
+     puts "n 是质数"
+   end
+   i+= 1
+
+  #  puts "#{i}"
+end
+
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
