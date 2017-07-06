1ea70a9905fd7dcf7b174a21c16c4dc74f7f781a
sww <admin@rails.com>
2017-07-06 18:14:03 +0800

    06-interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -5,6 +5,22 @@ print "请输入一个整数，然后按 Enter: "
 x = gets
 
 # ....
+a =
+if x.to_i > 0
+  "正数"
+elsif x.to_i == 0
+  "零"
+else
+  "负数"
+end
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+b =
+if x.to_i % 2 == 0
+  "偶数"
+else
+  "奇数"
+end
+
+
+puts "这个数是__#{a}___ "
+puts "这个数是__#{b}___ "
