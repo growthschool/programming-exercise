cf506b17b0e22147c3a198b2c2fc9c804a5de3e4
huangpuxiang <hpx0809@outlook.com>
2017-07-07 00:10:13 +0800

    24-hash-even

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -1,8 +1,14 @@
 # 给定一 Hash，输出 value 是偶数的 keys
 
 def find_even_keys(hash)
-
   # ... (请回传一个数组)
+  hh = []
+  hash.each do |key, value|
+    if value%2 == 0
+      hh << key
+    end
+  end
+  return hh
 
 end
 
@@ -17,5 +23,3 @@ h = {
 answer = find_even_keys(h)
 
 puts "有偶数 value 的 keys 有 #{answer}" # 应该是数组 [b,d,e]
-
-
