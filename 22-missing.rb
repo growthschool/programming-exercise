92b6b43242b043a6835a03d2f5d491aecd8beaa5
huangpuxiang <hpx0809@outlook.com>
2017-07-06 18:49:05 +0800

    22-missing

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,15 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  arrn = [0,1,2,3,4,5,6,7,8,9]
+    arr.each do |f|
+      arrn.each_with_index do |i, j|
+      if i == f
+        arrn.delete_at(j)
+      end
+    end
+  end
+  arrn
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
