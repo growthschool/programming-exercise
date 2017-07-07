c76a346e26d4a2f0dd11a7849aca586e5a87de84
Johnsonlee <johnsonlee28@gmail.com>
2017-07-07 09:56:30 +0800

    question 13

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,20 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
+n = n.to_i
+i = 0
+q = 0
+while (i <= n)
+  q = 0
+   while (q < n)
+     puts "#{i} x #{q} = #{i*q}"
+     q += 1
+   end
+   puts "#{i} x #{q} = #{i*q}"
+   i += 1
+ end
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
