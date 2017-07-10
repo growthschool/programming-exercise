489647ec014f36d581f2ee3200057d723defe54d
sww <admin@rails.com>
2017-07-10 19:36:19 +0800

    24-hash-even

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -3,8 +3,28 @@
 def find_even_keys(hash)
 
   # ... (请回传一个数组)
+  result = {}
+  hash.each do |key, value|
+    if value % 2 == 0
+      result[key] = value
+    end
+  end
+  result.keys
+
+  # ... (另一种方法)
+  # result_arr = []
+  # m = hash["a"]
+  # hash.each do |key, value|
+  #   if value % 2 == 0
+  #     m = value
+  #     result_arr << hash.key(m)
+  #   end
+  # end
+  # result_arr
 
 end
+
+
 
 h = {
   "a" => 71,
@@ -17,5 +37,3 @@ h = {
 answer = find_even_keys(h)
 
 puts "有偶数 value 的 keys 有 #{answer}" # 应该是数组 [b,d,e]
-
-
