fa21d2015e6bea883c6a2b1815cc41bad65b1bce
robote <2233628310@qq.com>
2017-07-10 20:15:23 +0800

    10-function  finished

--- 10-function.rb
+++ 10-function.rb
@@ -1,6 +1,14 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
 def find_max(x, y, z)
+  if  (x > y) && (y > z)
+    "x"
+  elsif  (y > x) &&  (x > z)
+    "y"
+  else
+    "z"
+  end
+
 end
 
 print "请输入一个数字x，然后按 Enter: "
