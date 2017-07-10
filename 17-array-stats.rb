4982ba0447b05e658613ab52ff510002b3986824
robote <2233628310@qq.com>
2017-07-10 22:33:46 +0800

    17-array-status  finished

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -12,9 +12,32 @@ while (true)
   end
 end
 
+def  find_max(array)
+  s =  array.sort
+  return  s.last
+end
+
+def  find_min(array)
+  s =  array.sort
+  return s.first
+end
+
+def  sum(array)
+  sum  =  0
+  array.each  do  |arr|
+    sum  +=  arr
+  end
+  return  sum
+end
+
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+sum  =  sum(arr)
+avg  =  sum.to_f/ arr.size
+max  =  find_max(arr)
+min  =  find_min(arr)
+
+puts "总和是 _#{sum}____"
+puts "平均是 __#{avg}___"
+puts "最大值是 _#{max}____"
+puts "最小值是 _#{min}___"
