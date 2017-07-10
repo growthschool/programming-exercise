6c3da25232c609c5a34ae6ca3a5bae7d114fc471
Your Name <you@example.com>
2017-07-10 11:07:23 +0800

    28-word-count.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,17 @@
 
 doc = File.read("wordcount.txt")
 
+arr = doc.split #现在是每个单词出现的概率arr = doc.split("")是每个字母出现的概率 
+
+h = {}
+
+arr.each do |d|
+  if h[d] == nil
+    h[d] = 1
+  else
+    h[d] += 1
+  end
+end
+
+puts h
 # ...
