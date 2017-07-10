5f726dcbb829261178580dd995e9f0936be54ee9
sww <admin@rails.com>
2017-07-10 19:51:50 +0800

    25-hash-count

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -5,6 +5,11 @@ def count(arr)
 
   arr.each do |i|
     # ...
+    if h[i] == nil
+       h[i] = 1
+    else
+        h[i] = h[i] + 1
+    end
   end
 
   return h # 回传一个 hash
@@ -15,4 +20,3 @@ arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
