7306efe42d5195889f6c837bbe15b9737424f676
JayGJianGuo <jkland927@gmail>
2017-07-11 13:42:03 +0800

    Answer 4

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -6,7 +6,12 @@ pizzas = gets
 print "请输入有多少人要吃，然后按 Enter: "
 people = gets
 
+pizzas = pizzas.to_i
+people = people.to_i
+
+people_gets = pizzas/people
+pizzas_left = pizzas%people
 # .....
 
-puts "每人可分得几片: _________ 片"
-puts "还剩下几片: _________ 片"
+puts "每人可分得几片: _____#{people_gets}____ 片"
+puts "还剩下几片: _____#{pizzas_left}____ 片"
