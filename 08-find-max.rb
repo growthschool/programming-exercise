52e8454778ac8987aa302a241b64b29330d2f8ba
xyy601chenyan <xyy545688506@gmail.com>
2017-07-11 14:54:50 +0800

    question8 finished

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -2,13 +2,27 @@
 
 print "请输入一个数字x，然后按 Enter: "
 x = gets
+x = x.to_f
 
 print "请输入一个数字y，然后按 Enter: "
 y = gets
+y = y.to_f
 
 print "请输入一个数字z，然后按 Enter: "
 z = gets
+z = z.to_f
 
-# ....
+a = [x,y,z]
 
-puts "最大的数是 ________(x或y或z)"
+max = a[0]
+
+for i in 1..2 do
+  if a[i]>= max
+    max = a[i]
+  else
+    max = max
+  end
+end
+
+
+puts "最大的数是 #{max}"
