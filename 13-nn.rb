11b0c6da0869aa87c59c772a5523c7be04ce91d3
JayGJianGuo <jkland927@gmail>
2017-07-11 15:41:49 +0800

    Answer 13

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,10 +2,20 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
-
+n = n.to_i
+i = 1
+j = 1
+while (i<=n)
+  while (j<=n)
+    sum = j*i
+    puts "#{i}*#{j}=#{sum}"
+    j+=1
+  end
+  j=1
+  i+=1
+end
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
