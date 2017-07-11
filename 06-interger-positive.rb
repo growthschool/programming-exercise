950870d0610da087d3f51069d4406a42486f36d8
JayGJianGuo <jkland927@gmail>
2017-07-11 13:56:06 +0800

    Answer 6

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -3,8 +3,27 @@
 
 print "请输入一个整数，然后按 Enter: "
 x = gets
+x = x.to_f
+state_1 = "nil"
+state_2 = "nil"
+
+if x == 0
+  state_1 = "零"
+elsif x > 0
+  state_1 = "正数（positive）"
+else
+  state_1 = "负数（negative）"
+end
+
+if state_1 == "零"
+  state_2 = "零"
+elsif x % 2 == 0
+  state_2 = "偶数"
+else
+  state_2 = "奇数"
+end
 
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+puts "这个数是___#{state_1}__ (正数或零或负数)"
+puts "这个数是___#{state_2}__ (偶数或奇数)"
