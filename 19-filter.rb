50d1e1abf3d827890b0be10267bcac3772139dae
JayGJianGuo <jkland927@gmail>
2017-07-11 19:49:32 +0800

    fail to think

--- 19-filter.rb
+++ 19-filter.rb
@@ -1,9 +1,20 @@
 # 给定一阵列内含数字，输出另一个数组只包含偶数
-
 def filter_even(arr)
-  #...
+  j = 0
+  for i in arr
+    if i % 2 == 0
+      arr.delete_at(j)
+    end
+    j = j 
+  end
+
+  #arr.each_with_index do |i, j|
+  #  if i % 2 != 0
+  #    arr.delete_at(j)
+  #  end
+  #end
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
-puts filter_even(arr).to_s # 应该是 [68, 42, 46, 46, 86]
+puts filter_even(arr) # 应该是 [68, 42, 46, 46, 86]
