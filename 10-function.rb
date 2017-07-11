3b377656ed3c27aa4214aabf7e8e1d9523588c8f
xyy601chenyan <xyy545688506@gmail.com>
2017-07-11 15:07:20 +0800

    question10 finished

--- 10-function.rb
+++ 10-function.rb
@@ -1,6 +1,16 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
 def find_max(x, y, z)
+  max = x
+  if y > x
+    max = y
+    if z > max
+      max = z
+    end
+  end
+  return max
+
+
 end
 
 print "请输入一个数字x，然后按 Enter: "
