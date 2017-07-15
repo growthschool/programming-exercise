cda7486791d836c77f10b0adfbe9a2f3daa554c1
jiaowagn <wangjiaohere@gmail.com>
2017-07-15 14:13:12 +0800

    28-word-count.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,17 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+h = {}
+
+doc.each_line { |line|
+    words = line.split
+    words.each { |w|
+        if h.has_key?(w)
+            h[w] = h[w] + 1
+            else
+            h[w] = 1
+        end
+    }
+}
+
+puts h
