5f70cfbf56cbd819abab30a03fdff864f25c2d57
JayGJianGuo <jkland927@gmail>
2017-07-16 12:19:06 +0800

    Answer 25

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -2,11 +2,26 @@
 
 def count(arr)
   h = {}
-
-  arr.each do |i|
-    # ...
+  arr_item = []
+  arr_item = arr.uniq
+  arr_counts = []
+  arr_item.each do |i|
+    counts = 0
+    arr.each do |j|
+      if j == i
+        counts+=1
+      end
+    end
+    arr_counts << counts
   end
-
+  puts arr_item.to_s
+  puts arr_counts.to_s
+  arr_item.each do |i|
+    arr_counts.each do |j|
+      h[i] = j
+      #h.values = arr_counts
+    end
+  end
   return h # 回传一个 hash
 end
 
@@ -15,4 +30,3 @@ arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
