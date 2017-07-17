8a3f75819691fb6edfe38596fa8da238e82c3991
like <20783506632@qq.com>
2017-07-17 10:11:01 +0800

    function

--- 09-function.rb
+++ 09-function.rb
@@ -1,14 +1,16 @@
 # 题目: 输入直角三角形的宽和高，输出三角形的面积
 
 def calculate_area(a, b)
-  75
+  area = a * b
 end
 
 print "请输入直角三角形的高，然后按 Enter: "
 a = gets
+a = a.to_f
 
 print "请输入直角三角形的底边，然后按 Enter: "
 b = gets
+b = b.to_f
 
 answer = calculate_area(a,b)
 
