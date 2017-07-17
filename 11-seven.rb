c144b42ef7781b54243ebbdf833b56d909ccc8f6
like <20783506632@qq.com>
2017-07-17 10:28:36 +0800

    11-seven

--- 11-seven.rb
+++ 11-seven.rb
@@ -1,15 +1,11 @@
 # 题目: 列出 1 到 100 之间，所有 7 的倍数
 
-i = 7
+i = 1
 while i <= 100
-    puts i
-    # i = i*7
-    i = i + 7
-  # i+=1
+    if i % 7 == 0
+    	puts i
 end
 
-# x = 1
-# while x <= 10
-#   puts x
-#   x = x + 1
-# end
+  i += 1
+
+ end
