352014fb159c7505ebc0190152ee2e0da37d7a38
zpf0918 <zhanggongzifei@163.com>
2017-07-18 15:05:13 +0800

    implement 14

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,7 +1,14 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  x = 2
+  while x < n/2
+    if n % 2 == 0
+       return false
+     else
+       return true
+    end
+  end
 end
 
 print "请输入数字 N，然后按 Enter: "
