306539bc37493ea7eb142b4014d21d293ab2dd50
Ji Xiang <jx4554@gmail.com>
2017-07-18 08:58:47 +0800

    finish 17

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,7 +1,13 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  #....
+	max = array.first
+	(2..array.size).to_a.each do |i|
+		if max < array[i-1]
+			max = array[i-1]
+		end
+	end
+	return max
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -14,7 +14,7 @@ end
 
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts "总和是 #{arr.sum}"
+puts "平均是 #{arr.sum.to_f/arr.size}"
+puts "最大值是 #{arr.max}"
+puts "最小值是 #{arr.min}"
