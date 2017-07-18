511c531b8ae7f5290c317a5f9b89f59529cc5c90
Kerzzi <kerzzi@outlook.com>
2017-07-18 18:47:26 +0800

    finish 04

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -1,12 +1,16 @@
 # 题目: 输入有多少片比萨饼和多少人，输出每人可以分到几片，以及剩下几片
 
 print "请输入有多少片比萨饼，然后按 Enter: "
-pizzas = gets
+pizzas = gets.to_i
 
 print "请输入有多少人要吃，然后按 Enter: "
-people = gets
+people = gets.to_i
 
 # .....
 
-puts "每人可分得几片: _________ 片"
-puts "还剩下几片: _________ 片"
+average = (pizzas/people).to_i
+
+remainder = (pizzas % people).to_i
+
+puts "每人可分得几片: #{average} 片"
+puts "还剩下几片: #{remainder} 片"
