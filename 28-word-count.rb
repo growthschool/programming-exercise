ea20fe3c4a9cf029fc5987f5f8904cdf2e98e42d
Ji Xiang <jx4554@gmail.com>
2017-07-18 10:42:25 +0800

    finish 28

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,13 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+dic = {}
+
+doc.each_line do |line|
+	line = line.chomp
+	line.each_char do |char|
+		dic[char] ? dic[char] += 1 : dic[char] = 1
+	end
+end
+
+puts dic
