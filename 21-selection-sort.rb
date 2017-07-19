796d314ac0b482dbc84d2c82d6d4580b0d29a6b2
Kerzzi <kerzzi@outlook.com>
2017-07-19 19:56:06 +0800

    finish 21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,8 +1,14 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
+
 def insertion_sort(arr)
-  #...
+  (0...arr.length).each do |i|
+    min, index = arr[i], i
+    (i...arr.length).each { |j| min, index = arr[j], j if arr[j] < min }
+    arr[i], arr[index] = arr[index], arr[i]
+  end
+  arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
