caaade38b523e72256c138c542597b7a7a596ce6
zhuqi <ardentyouny>
2017-07-19 14:34:14 +0800

    complete 05

--- 05-bmi.rb
+++ 05-bmi.rb
@@ -9,9 +9,15 @@ weight = gets
 
 print "请输入您的身高(厘米)，然后按 Enter: "
 height = gets
-
+a = 10000*weight.to_f/(height.to_f*height.to_f)
 # .....
 
-puts "您的 BMI 是: _________"
+puts "您的 BMI 是: #{a}"
 
-puts "您的 BMI 结果是: _________(过轻或正常或过重)"
+if a<18.5
+puts "您的 BMI 结果是: 过轻"
+elsif a>=24
+  puts "您的 BMI 结果是: 过重"
+else
+  puts "您的 BMI 结果是: 正常"
+end
