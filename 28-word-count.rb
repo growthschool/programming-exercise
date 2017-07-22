9f49b95c9da6ac2967d4981290f2cb22fee22df3
Kerzzi <kerzzi@outlook.com>
2017-07-22 20:11:39 +0800

    finish 28

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,29 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+words = doc.downcase.scan(/\w+/)
+
+result = {}
+
+words.each do |i|
+  result[i] = words.count(i)
+end
+
+puts "每个单字出现的次数为:#{result}"
+
+
+
+# doc = File.read("wordcount.txt")
+# words = doc.downcase.scan(/\w+/)
+#   def word_count(words)
+#     f = {}
+#     words.each do |word|
+#       count = words.count(word)
+#       f[word] = count
+#     end
+#     return f
+#   end
+#
+# result = word_count(words)
+#
+# puts "每个单字出现的次数为:#{result}"
