14ae2a25e28c0c6d4acb0ede42a208f54c8794cc
zhuqi <ardentyouny>
2017-07-23 17:45:09 +0800

    complete 23

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -1,7 +1,13 @@
 # 给定一 Hash，输出有最大 value 的 key
 
 def find_max(hash)
-  # ...
+  a = 0
+  hash.each do |key,value|
+    if a <= value
+      a = value
+    end
+  end
+  return hash.key(a)
 end
 
 h = {
@@ -15,5 +21,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
