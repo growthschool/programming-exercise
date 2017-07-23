787708f23c8ff02a28e6033f3c2ae6247708e336
zhuqi <ardentyouny>
2017-07-23 11:33:21 +0800

    complete 17

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -1,5 +1,52 @@
 # 使用者不断输入数字存进 Array，最后输出总和、平均、最大值、最小值
 
+def total(array)
+  a = 0
+  array.each do |i|
+    a = a + i
+  end
+  return a
+end
+
+def average(array)
+  a = 0
+  b = 0
+  array.each do |i|
+    a = a + i
+    b = b + 1
+  end
+  c = a/b
+  return c
+end
+
+def max(array)
+  a = array[0]
+  array.each do |i|
+    if a>=i
+      a = a
+    else
+      a = i
+    end
+  end
+    return a
+end
+
+def min(array)
+  a = array[0]
+  array.each do |i|
+    if a <= i
+      a = a
+    else
+      a = i
+    end
+  end
+  return a
+end
+
+
+
+
+
 arr = []
 
 while (true)
@@ -12,9 +59,9 @@ while (true)
   end
 end
 
-puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts arr.to_s
+puts "总和是 #{total(arr)}"
+puts "平均是 #{average(arr)}"
+puts "最大值是 #{max(arr)}"
+puts "最小值是 #{min(arr)}"
