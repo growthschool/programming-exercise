05950c4976a6a7030149016513c712417f966cce
cosyfish <steven-jiang@foxmail.com>
2017-07-23 16:37:14 +0800

    4

--- 05-bmi.rb
+++ 05-bmi.rb
@@ -5,13 +5,23 @@
 # 如果 BMI 介于 18.5 ~ 24，显示正常
 
 print "请输入您的体重(公斤)，然后按 Enter: "
-weight = gets
+weight = gets.to_f
 
 print "请输入您的身高(厘米)，然后按 Enter: "
-height = gets
+height = gets.to_f
 
-# .....
+print "请输入您的的姓名"
+name = gets
 
-puts "您的 BMI 是: _________"
+bmi = weight/(height * height)
 
-puts "您的 BMI 结果是: _________(过轻或正常或过重)"
+puts "您的 BMI 是: #{bmi}"
+if bmi < 18.5
+  print "你的体重过轻"
+elsif bmi >= 24
+  print "你的体重过重"
+elsif 18.5< bmi <= 24
+  print "您的体重正常"
+end
+
+puts "#{name}你好，您的 BMI 结果是: _________(过轻或正常或过重)"
