fe7d2a12c50af2c402fef0dba5dfa73f8d856271
nathanye <khalil54781@gmail.com>
2017-07-24 15:44:59 +0800

    07

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -9,14 +9,30 @@
 #     当 z < 0 输出 "E"
 
 print "请输入一个整数x，然后按 Enter: "
-x = gets
+x = gets().to_i
 
 print "请输入一个整数y，然后按 Enter: "
-y = gets
+y = gets().to_i
 
 print "请输入一个整数z，然后按 Enter: "
-z = gets
+z = gets().to_i
 
-# ....
+if x < 0
+  r = "A"
+else
+  if y > 0
+    if z > 0
+      r = "B"
+    else
+      r = "C"
+    end
+  else
+    if z > 0
+      r = "D"
+    else
+      r = "E"
+    end
+  end
+end
 
-puts "结果是________(A或B或C或D或E)"
+puts "结果是 #{r}"
