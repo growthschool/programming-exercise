9d265f9d264701071fd7da2604ff0fd1cfabf10f
lucas <d2194499406@gmail.com>
2017-07-24 19:46:46 +0800

    puts a b c d e can be true

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -19,4 +19,16 @@ z = gets
 
 # ....
 
-puts "结果是________(A或B或C或D或E)"
+if "#{x.to_i}" < "0"
+  puts "A"
+else
+  if "#{y.to_i}" > "0" && "#{z.to_i}" > "0"
+    puts "B"
+  elsif "#{y.to_i}" > "0" && "#{z.to_i }"< "0"
+    puts "C"
+  elsif "#{y.to_i}" < "0" && "#{ z.to_i}" < "0"
+    puts "E"
+  elsif "#{y.to_i}" < "0" && "#{z.to_i}" > "0"
+    puts "D"
+  end
+end
