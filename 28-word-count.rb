763542cd03c05d0897768454a0f97dedfd04ae15
Johnsonlee <johnsonlee28@gmail.com>
2017-07-24 11:56:33 +0800

    question 27

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,28 @@
 
 doc = File.read("wordcount.txt")
 
+def count(arr)
+  h = {}
+  i = 0
+  n = arr.size
+
+  while (i < n)
+    a = arr.first
+    value = 0
+    arr.each do |i|
+      if a == i
+        value += 1
+      end
+      h[i] = value
+    end
+    arr = arr - [a]
+    i += 1
+  end
+
+  return h
+end
+
+answer = count(doc.split(" "))
+
+puts answer
 # ...
