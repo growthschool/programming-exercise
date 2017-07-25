40f316815e257598ab019de8698bfe8ef8515b32
lihaihao <mashy1024@gmail.com>
2017-07-25 11:51:28 +0800

    finish 07

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -19,4 +19,24 @@ z = gets
 
 # ....
 
-puts "结果是________(A或B或C或D或E)"
+x = x.to_i
+y = y.to_i
+z = z.to_i
+
+if x < 0
+  result = "A"
+elsif x > 0 && y > 0 && z > 0
+  result = "B"
+elsif x > 0 && y > 0 && z < 0
+  result = "C"
+elsif x > 0 && y < 0 && z > 0
+  result = "D"
+elsif x > 0 && y < 0 && z < 0
+  result = "E"
+else
+  result = "请输入非零的数字"
+end
+
+
+
+puts "结果是#{result}"
