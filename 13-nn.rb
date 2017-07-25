80433d06a2e9e5bd895be59e5acfa509b167432c
lihaihao <mashy1024@gmail.com>
2017-07-25 23:08:14 +0800

    finish 13

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,10 +2,22 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
+n = n.to_i
+
+a = 0
+while a <= n
+  b = 0
+  while b <= n
+    puts "#{a} x #{b} = #{a*b}"
+    b += 1
+  end
+  a += 1
+end
+
+
 
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
