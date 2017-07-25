6aab383cbf84a7fb0e6d85876a4d47ce4b063b1e
zhuqi <ardentyouny>
2017-07-25 09:54:26 +0800

    complete 25

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -2,12 +2,18 @@
 
 def count(arr)
   h = {}
-
-  arr.each do |i|
-    # ...
-  end
-
-  return h # 回传一个 hash
+  y = []
+  y = arr.uniq
+    y.each do |j|
+      a = 0
+        arr.each do |i|
+          if j == i
+            a = a + 1
+          end
+        end
+        h[j] = a
+      end
+      return h
 end
 
 arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a", "c", "e", "a", "d", "e"]
@@ -15,4 +21,3 @@ arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
