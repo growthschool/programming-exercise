fd14ce5172cf78b912a96ff640af4912c095d70c
dataiyung <dataiyung@gmail.com>
2017-07-26 10:05:25 +0800

    Q4 pizza

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -5,8 +5,9 @@ pizzas = gets
 
 print "请输入有多少人要吃，然后按 Enter: "
 people = gets
+#設x為每個人可得多少片 y為剩下幾片
+x= pizzas.to_i / people.to_i #先把pizza的字串改成整數
+y= pizzas.to_i % people.to_i  #用%取餘數
 
-# .....
-
-puts "每人可分得几片: _________ 片"
-puts "还剩下几片: _________ 片"
+puts "每人可分得几片: ____#{x}_____ 片"
+puts "还剩下几片: ____#{y}_____ 片"
