7be0b5d51d7ec1635b39697f1436d0d31201dc69
AnattaGuo <mengjunkwok@gmail.com>
2017-07-26 19:44:40 +0800

    guess-number

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,11 +1,18 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  i = 2
+end
+
+  while ( i <= n-1 )
+    returen false if n <= 1 || n % i == 0
+    i += 1
+  end
+  true
 end
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets().to_i
 
 if is_prime(n.to_i)
   puts "这是质数"
