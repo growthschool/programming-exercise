8607b111559d4436f4856a2b42bdebf511204db0
Yopiluv <wwnchxz@hotmail.com>
2017-07-27 03:50:52 +0800

    数组Array

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -2,10 +2,16 @@
 
 def find_max(array)
   #....
+  m = array[0]
+  array.each do |i|
+    if m <= i
+      m = i
+    end
+  end
+  m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
--- 20-sorting.rb
+++ 20-sorting.rb
@@ -3,9 +3,16 @@
 
 def filter_even(arr)
   #...
+  even_arr = []
+  arr.each do |i|
+    if (i % 2 == 0)
+      even_arr.push(i)
+    end
+  end
+  even_arr.uniq.sort
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
 
-puts "________" # 应该是 [42, 46, 68, 86]
+puts filter_even(arr).to_s # 应该是 [42, 46, 68, 86]
