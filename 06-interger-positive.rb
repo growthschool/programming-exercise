9002cb283f4af779e7033f75437f9bd9d9dbe76b
dataiyung <dataiyung@gmail.com>
2017-07-26 11:12:53 +0800

    Q6interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -3,8 +3,37 @@
 
 print "请输入一个整数，然后按 Enter: "
 x = gets
+x =x.to_f
+if x < 0
+  y = "負數"
+elsif x > 0
+  y = "正數"
+else
+  y = "零"
+end
 
-# ....
+if x % 2 == 0
+z = '偶數'
+else
+  z = "奇數"
+end
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+
+
+puts "这个数是#{y} (正数或零或负数)"
+puts "这个数是#{z} (偶数或奇数)"
+# x = gets().to_f
+#
+# if x > 0
+#   result1 = "正數"
+# elsif x==0
+#   result1 = "零"
+# else
+#   result1 = "負數"
+# end
+#
+# result2 = x % 2 == 0? "偶數" : "奇數"
+#
+# puts "#{x}"
+# puts "這個數是 #{result1}"
+# puts "這個數是 #{result2}"
