75079903a0bb6232d0a32253b8f0fc11184b9d64
lucas <d2194499406@gmail.com>
2017-07-26 11:12:03 +0800

    18-square be sure

--- 18-square.rb
+++ 18-square.rb
@@ -1,10 +1,16 @@
 # 建构一个阵列有一百个的元素，内容是 0, 1, 4, 9, 16, 25...... 每个元素是该索引的平方
 
-arr = []
+arr = [0]
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
 # ...
+i = 0
+for i in 0..(n.to_i-2)
+  i += 1
+  j = "#{(i.to_i * i.to_i)}"
+  arr.push(j)
+end
 
 puts arr.to_s
