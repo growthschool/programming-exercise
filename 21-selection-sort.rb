16819022bcfafcb9c5992920fc34f7ad711d5768
JayGJianGuo <jkland927@gmail>
2017-07-28 13:51:11 +0900

    Answer 21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,30 +1,14 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
-def insertion_sort(arr)
-  min = 0
-  box = 0
-  box_1 = 0
-  length = arr.size
-  j = 0
-    while j < length
-      min_index = j
-      k = j+1
-      while k < length - 1
-        if arr[min_index] > arr[min_index+1]
-          min_index = k
-          box = arr[min_index]
-          box_1 = arr[k]
-          arr[min_index] = box_1
-          arr[k] = box
-          j+=1
-        else
-          arr[min_index]
-          arr[k] = arr[min_index+1]
-        end
-      end
+def insertion_sort(array)
+  result = []
+  array.size.times do |i|
+    min = array.min
+    result << min
+    array.delete_at(array.index(min))
   end
-  return arr
+  return result
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
