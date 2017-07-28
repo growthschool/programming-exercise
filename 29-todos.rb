2bffa88442ed7efbec467a0694100fdb79f0ff6f
JayGJianGuo <jkland927@gmail>
2017-07-28 13:57:06 +0900

    Answer 29.copy

--- 29-todos.rb
+++ 29-todos.rb
@@ -1,4 +1,9 @@
 # 简易 Todo 代办事项应用
+def show(array)
+  array.each_with_index do |todo, index|
+    puts "#{index}: #{todo}"
+  end
+end
 
 text = File.read("todos.txt")
 
@@ -7,9 +12,7 @@ text.each_line do |line|
   todos << line.chomp
 end
 
-todos.each_with_index do |todo, index|
-  puts "#{index}: #{todo}"
-end
+show(todos)
 
 while (true)
   print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
@@ -17,17 +20,60 @@ while (true)
 
   if command == "add"
     print "请输入代办事项: "
-    # ...
+
+    todos << gets.chomp
+    show(todos)
+
   elsif command == "remove"
     print "请输入要删除的编号: "
-    # ...
+    #
+    i = gets.chomp.to_i
+    todos.delete_at(i)
+    show(todos)
   elsif command == "save"
     puts "存盘离开"
 
     # ...
+    File.open("todos.txt", "w+") do |f|
+      todos.each do |t|
+        f << t + "\n"
+      end
+    end
+
     break;
   else
     puts "看不懂，请再输入一次"
   end
 end
 
+#
+#text = File.read("todos.txt")
+#
+#todos = []
+#text.each_line do |line|
+#  todos << line.chomp
+#end
+#
+#todos.each_with_index do |todo, index|
+#  puts "#{index}: #{todo}"
+#end
+#
+#while (true)
+#  print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
+#  command = gets.chomp
+#
+#  if command == "add"
+#    print "请输入代办事项: "
+#    # ...
+#  elsif command == "remove"
+#    print "请输入要删除的编号: "
+#    # ...
+#  elsif command == "save"
+#    puts "存盘离开"
+#
+    # ...
+#    break;
+#  else
+#    puts "看不懂，请再输入一次"
+#  end
+#end
