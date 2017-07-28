f1b127d937452293c9fd67b8e52dd9fd6e0920af
JayGJianGuo <jkland927@gmail>
2017-07-28 13:55:02 +0900

    Answer 28.copy

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -3,3 +3,21 @@
 doc = File.read("wordcount.txt")
 
 # ...
+base = doc.downcase.scan(/\w+/)
+
+def count(arr)
+  h = {}
+  x = []
+  x = arr.uniq
+
+  x.each do |i|
+    y = arr.count(i)
+    h[i] = y
+  end
+
+  return h # 回传一个 hash
+end
+
+result = count(base)
+
+puts result
