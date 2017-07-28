17470b6cb489ace356ce5c828cdd81fb77621056
dataiyung <dataiyung@gmail.com>
2017-07-28 19:00:03 +0800

    Q26 hash_filter

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -1,5 +1,4 @@
 # 给定一个数组包含 Hash，请过滤和排序
-
 arr = [
   { "name" => "Peter", "age" => 30 },
   { "name" => "John", "age" => 15 },
@@ -8,9 +7,29 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
+def filter(arr)
+  result_arr = []
+
+
+ arr.each do |h|
+   if h["age"] >= 18
+     result_arr.push(h)
+   end
+ end
+
+ result_arr.sort_by { |i| i["age"]}
+
+ end
+
+  puts "所有成年人，并由小到大: #{filter(arr)}"
+
+
+
+
+
+
 # ....
 
-puts "所有成年人，并由小到大: _________"
 
 # 答案应该是
 #[
