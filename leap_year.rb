c47c8f9160c107e421913e33307ace5f98bc1538
郭春日 <uochunri@gmail.com>
2017-07-29 14:22:09 +0800

    modify is_leap_year code

--- leap_year.rb
+++ leap_year.rb
@@ -8,18 +8,24 @@
 
 =end
 
-def is_leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0
-      if year % 400 == 0
-        return true
-      else
-        return false
-      end
-    else
-      return true
-    end
-  else
-    return false
-  end
+# method 1:
+# def is_leap_year?(year)
+#   if year % 4 == 0
+#     if year % 100 == 0
+#       if year % 400 == 0
+#         return true
+#       else
+#         return false
+#       end
+#     else
+#       return true
+#     end
+#   else
+#     return false
+#   end
+# end
+
+# method 2:
+def is_leap_year?(y)
+  y % 400 == 0 || ( y % 4 == 0 && y % 100 != 0)
 end
