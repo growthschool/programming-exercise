b785c8114ad69815c360bb4c77eb5868d23df0b7
sihhan <thehappyend95@gmail.com>
2017-07-30 21:43:24 +0800

    14

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,7 +1,15 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  i = 2
+     while (i ** 2 <= n) do
+       if n % i == 0
+         return false
+       else
+         i = 1
+       end
+     end
+     true
 end
 
 print "请输入数字 N，然后按 Enter: "
