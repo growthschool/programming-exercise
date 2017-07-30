672c3a0b08ba80787965967e1de03b4a6a89dbe6
sihhan <thehappyend95@gmail.com>
2017-07-30 21:48:47 +0800

    16

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,7 +1,14 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  #....
+    m = array[0]
+   for i in (1..array.length-1) do
+     if m < array[i]
+       m = array[i]
+     end
+     i = 1
+   end
+   m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
