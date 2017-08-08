5ac6e97e884f3858d3a317a34e7543a585295ca5
lichen <119449234@qq.com>
2017-08-08 23:11:26 +0800

    class

--- 27-class.rb
+++ 27-class.rb
@@ -1,5 +1,10 @@
 class Person
-  # ...
+  attr_accessor :first_name, :last_name
+
+  def greet
+    puts "Hi, #{first_name} #{last_name}"
+    puts "Hi, #{@first_name} #{@last_name}"     #上面的另一种有效写法
+  end
 end
 
 p1 = Person.new
@@ -11,6 +16,3 @@ p2 = Person.new
 p2.first_name = "William"
 p2.last_name = "Zhang"
 p2.greet # 输出 "Hello, William Zhang"
-
-
-
