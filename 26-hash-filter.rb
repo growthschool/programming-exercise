23c0c3019b7a4dd77740b0a69c0c6616747d3699
lichen <119449234@qq.com>
2017-08-08 22:27:01 +0800

    hash filter

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -1,5 +1,6 @@
 # 给定一个数组包含 Hash，请过滤和排序
 
+#方法 1
 arr = [
   { "name" => "Peter", "age" => 30 },
   { "name" => "John", "age" => 15 },
@@ -8,9 +9,41 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
-# ....
+def filter(arr)
+  adults = []
+  arr.each do |i|
+    if i["age"] >= 18
+      adults.push(i)
+      # adults << i     #也是可以的
+    end
+  end
+  adults.sort_by { |j| j["age"] }
+end
 
-puts "所有成年人，并由小到大: _________"
+puts "所有成年人，并由小到大: #{filter(arr)}"
+
+# 答案应该是
+#[
+#  { "name" => "Steven", "age" => 22 },
+#  { "name" => "Peter", "age" => 30 },
+#  { "name" => "David", "age" => 45 }
+#]
+
+#方法 2
+arr = [
+  { "name" => "Peter", "age" => 30 },
+  { "name" => "John", "age" => 15 },
+  { "name" => "David", "age" => 45 },
+  { "name" => "Steven", "age" => 22 },
+  { "name" => "Vincent", "age" => 6 },
+]
+
+def filter(arr)
+  adults = arr.select{|i| i["age"] >= 18}
+  adults.sort_by { |j| j["age"] }
+end
+
+puts "所有成年人，并由小到大: #{filter(arr)}"
 
 # 答案应该是
 #[
