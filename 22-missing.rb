d47ba8a17378b4401f9455ed138ab38b5b025ef5
meganho <bbeeaanns@gmail.com>
2017-08-10 16:41:03 +0200

    22-missing.rb

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,13 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  missing_num = []
+
+  (0..9).each do |i|
+    missing_num << i unless arr.include?(i)
+  end
+
+  missing_num
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
