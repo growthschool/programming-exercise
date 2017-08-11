d3a41dee9c7d8efd420a7fc5d007a2996da789ca
wangjingru <w15673142360@gmail.com>
2017-08-11 20:23:34 +0800

    完成21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,19 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  i = 0
+  while i < (arr.size - 1)
+    j = i + 1
+
+    while j < arr.size
+      if arr[i] > arr[j]
+        arr[i], arr[j] = arr[j], arr[i]
+      end
+      j += 1
+    end
+    i += 1
+  end
+  arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
