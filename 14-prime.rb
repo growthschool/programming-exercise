2930f0f139c42bfb40410cc56449efd9a072447b
jkxruby <jkxruby@gmail.com>
2017-08-18 11:08:59 +0800

    subject14

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,11 +1,21 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
-end
+i = 2
+while (i<=(n / 2))
+return false if n <= 1 || n % i == 0
+     i += 1
+  end
+   true
+
+ end
+
+
+
+
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets.to_i
 
 if is_prime(n.to_i)
   puts "这是质数"
