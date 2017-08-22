e5bc78e05c77369961d05fe7a48ba519e84811e2
Calvin <xcalvin.zhao@gmail.com>
2017-08-22 22:09:43 +0800

    题目7

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -2,9 +2,15 @@
 
 
 print "请输入一个整数，然后按 Enter: "
-x = gets
+x = gets().to_i
 
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if x > 0 then puts "这个数是正数"
+elsif x == 0 then puts "这个数是零"
+else puts "这个数是负数"
+end
+
+if x % 2 == 0 then puts "这个数是偶数"
+else puts "这个数是奇数"
+end
