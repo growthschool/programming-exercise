931099bfe17e42a01734387c12fc12278465c516
johsnow <johsnow08@gmail.com>
2017-08-23 22:11:21 +0800

    work23 done

--- 23-hash-max.rb
+++ 23-hash-max.rb
@@ -1,7 +1,12 @@
 # 给定一 Hash，输出有最大 value 的 key
 
 def find_max(hash)
-  # ...
+  arr = hash.values
+  hash.each do |key, value|
+    if value == arr.max
+      return key
+    end
+  end
 end
 
 h = {
@@ -15,5 +20,3 @@ h = {
 answer = find_max(h)
 
 puts "有最大 value 的是 #{answer}" # 应该是 d
-
-
