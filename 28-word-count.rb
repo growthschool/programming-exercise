85397c73e3ac9b298012fa8a2bd34822f023fcb6
jkxruby <jkxruby@gmail.com>
2017-08-23 17:02:37 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,21 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
 doc = File.read("wordcount.txt")
+words = doc.downcase.scan(/\w+/)
 
-# ...
+result = words.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1}
+puts result
+
+# 另一种解法
+
+# arr = doc.split(" ")
+# h = {}
+# arr.each do |a|
+#   if h[a] == nil
+#     h[a] = 1
+#   else
+#     h[a] +=1
+#   end
+# end
+#
+# puts h
