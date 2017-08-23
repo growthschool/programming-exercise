511cdbbda5f499cff9d723a071c8eefc2967588b
韦月炎 <weiyueyan@MBP.local>
2017-08-24 00:06:34 +0800

    04-pizzas.rb

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -6,7 +6,9 @@ pizzas = gets
 print "请输入有多少人要吃，然后按 Enter: "
 people = gets
 
+a = pizzas.to_i/people.to_i
+b = pizzas.to_f % people.to_i
 # .....
 
-puts "每人可分得几片: _________ 片"
-puts "还剩下几片: _________ 片"
+puts "每人可分得几片: ____#{a}_____ 片"
+puts "还剩下几片: ____#{b}_____ 片"
