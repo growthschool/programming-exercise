2af9f3d7f5558df8c8d240e279460fb63dd5bbea
windf1sh <hy417595465@gmail.com>
2017-08-24 09:49:59 +0800

    20

--- 20-sorting.rb
+++ 20-sorting.rb
@@ -2,10 +2,20 @@
 # Hint: 可用 arr.sort 排序，和 arr.uniq 去除重复
 
 def filter_even(arr)
-  #...
+  arr1 = []
+  i = 0
+  while i < 10
+    if arr[i] % 2 == 0
+      arr1 = arr1.push(arr[i])
+    end
+    i = i + 1
+  end
+  arr1 = arr1
+  arr1 = arr1.uniq
+  arr1 = arr1.sort
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
 
-puts "________" # 应该是 [42, 46, 68, 86]
+puts "#{filter_even(arr)}" # 应该是 [42, 46, 68, 86]
