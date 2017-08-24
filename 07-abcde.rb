f133a3586fcbd8ace7d987bcb2ec94043e473d98
韦月炎 <weiyueyan@MBP.local>
2017-08-24 17:59:27 +0800

    07-abcde.rb

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -9,14 +9,32 @@
 #     当 z < 0 输出 "E"
 
 print "请输入一个整数x，然后按 Enter: "
-x = gets
+x = gets.to_i
 
 print "请输入一个整数y，然后按 Enter: "
-y = gets
+y = gets.to_i
 
 print "请输入一个整数z，然后按 Enter: "
-z = gets
+z = gets.to_i
 
+
+ if x < 0
+   t ="A"
+elsif x > 0
+  if y > 0
+    if z > 0
+      t = "B"
+    elsif z < 0
+      t = "C"
+    end
+  elsif y < 0
+    if z > 0
+      t = "D"
+    elsif z < 0
+      t = "E"
+    end
+  end
+ end
 # ....
 
-puts "结果是________(A或B或C或D或E)"
+puts "结果是___#{t}_____(A或B或C或D或E)"
