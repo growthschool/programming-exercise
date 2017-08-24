7a861ef3215c1c0a46c7d1f7ca70507a80c2b618
baolong <baolong@baolongdeMacBook-Air.local>
2017-08-24 16:43:29 +0800

    14

--- 13-nn.rb
+++ 13-nn.rb
@@ -2,10 +2,15 @@
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
+a = 0
+b = 0
 
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
 
+while a <= n.to_i
+  while b<= n.to_i
+    puts "#{a} x #{b} = #{a*b}"
+    b += 1
+  end
+  a += 1
+  b = 0
+end
