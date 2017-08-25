19fa2c727d5d53e23f27133f1a296b5b9f63f1b3
韦月炎 <weiyueyan@MBP.local>
2017-08-25 18:29:34 +0800

    17-array-stats.rb

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -12,9 +12,48 @@ while (true)
   end
 end
 
+
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+def total(array)
+  t = 0
+  array.each do |i|
+    t = t + i
+  end
+    return t
+end
+
+def average(array)
+  a = total(array)
+  b = total(array)/array.size
+end
+
+def find_max(array)
+  x = 0
+  array.each do |i|
+    if i > x
+        x = i
+    end
+ end
+ return x
+end
+
+def find_min(array)
+  x = array[0]
+  array.each do |i|
+    if i < x
+       x = i
+    end
+  end
+  return x
+end
+
+max = find_max(arr)
+min = find_min(arr)
+
+puts "总和是 _#{total(arr)}____"
+puts "平均是 __#{average(arr)}___"
+puts "最大值是 _#{max}___"
+puts "最小值是 _#{min}____"
+
+#也可以直接调用数组对象的max(最大)和min(最小)方法
