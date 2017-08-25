b030f5152bbbd0660a5b6fc3cd4f3d35b63da5c7
ranty <ranty720@gmail.com>
2017-08-25 12:46:17 +0800

    08-find-max

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -6,9 +6,10 @@ x = gets
 print "请输入一个数字y，然后按 Enter: "
 y = gets
 
-print "请输入一个数字z，然后按 Enter: "
+print "请输入一个数nn 字z，然后按 Enter: "
 z = gets
 
 # ....
 
-puts "最大的数是 ________(x或y或z)"
+result = [x, y, z].max
+puts "最大的数是 #{result}"
