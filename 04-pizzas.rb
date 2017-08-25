7b2e013a2075b6b24e8764c01a9146e1fe6e740d
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-25 14:24:16 +0800

    ok 04

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -2,11 +2,14 @@
 
 print "请输入有多少片比萨饼，然后按 Enter: "
 pizzas = gets
+pizzas = pizzas.to_i
 
 print "请输入有多少人要吃，然后按 Enter: "
 people = gets
+people = people.to_i
 
+eat = pizzas/people
 # .....
 
-puts "每人可分得几片: _________ 片"
-puts "还剩下几片: _________ 片"
+puts "每人可分得几片: ___#{eat}___ 片"
+puts "还剩下几片: ___#{pizzas-(eat*people)}___ 片"
