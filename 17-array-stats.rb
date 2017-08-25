f945e4351d1345c727cd747ed9c96885faf330bb
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-25 15:56:31 +0800

    ok 17

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -12,9 +12,23 @@ while (true)
   end
 end
 
+max = arr[0]
+ min = arr[0]
+ sum = 0
+
+ arr.each do |i|
+     if max < i
+         max = i
+     end
+     if min > i
+         min = i
+     end
+     sum = sum + i
+ end
+
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts "总和是 _  #{sum}  _"
+ puts "平均是 _  #{sum/arr.size} _"
+ puts "最大值是 _ #{max} _"
+ puts "最小值是 __ #{min} _"
