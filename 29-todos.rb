a47ca7100b932eb66d3200ed816e397da96f2042
windf1sh <hy417595465@gmail.com>
2017-08-25 17:28:54 +0800

    29

--- 29-todos.rb
+++ 29-todos.rb
@@ -17,17 +17,21 @@ while (true)
 
   if command == "add"
     print "请输入代办事项: "
-    # ...
+    todos << gets.chomp
   elsif command == "remove"
     print "请输入要删除的编号: "
-    # ...
+    remove_no = (gets.chomp).to_i
+    todos.delete_at(remove_no)
   elsif command == "save"
     puts "存盘离开"
-
-    # ...
+    target = File.open("todos.txt", "w")
+    todos.each do |i|
+      target.write(i)
+      target.write("\n")
+    end
+    target.close()
     break;
   else
     puts "看不懂，请再输入一次"
   end
 end
-
