d0f468a0bf7af7574cbfca24b53ebdf5d181137d
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-25 14:16:41 +0800

    ok 03

--- 03-triangle.rb
+++ 03-triangle.rb
@@ -2,10 +2,13 @@
 
 print "请输入直角三角形的高，然后按 Enter: "
 a = gets
+a = a.to_f
 
 print "请输入直角三角形的底边，然后按 Enter: "
 b = gets
+b = b.to_f
 
+c = (a*b)/2
 # .....
 
-puts "直角三角形的面积是: _________"
+puts "直角三角形的面积是:__#{c}__"
