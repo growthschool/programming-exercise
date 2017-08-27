b30cea0cf4a9b2ae369b1dff2e4378836b972742
韦月炎 <weiyueyan@MBP.local>
2017-08-28 00:01:00 +0800

    26-hash-filter.rb

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,9 +8,29 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
+def under_age(arr)
+  array = []
+  k = 0
+  x = 0
+  while x < arr.size
+    a = arr[0]["age"]
+    arr.each_with_index do |i, j|
+      if i["age"] <= a
+          a = i["age"]
+          k = j
+      end
+    end
+    if arr[k]["age"] > 18
+      array << arr[k]
+    end
+    arr.delete_at(k)
+  end
+  return array
+end
+
 # ....
 
-puts "所有成年人，并由小到大: _________"
+puts "所有成年人，并由小到大: _#{under_age(arr)}________"
 
 # 答案应该是
 #[
