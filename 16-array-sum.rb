5ed4200b1ebb8579b4ede37a150e360c52160bd5
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-30 20:20:38 +0800

    16

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -2,10 +2,18 @@
 
 def find_max(array)
   #....
+	i=0
+	m=0
+	while i <(array.length)
+		if m<array[i]
+			m=array[i]
+		end
+		i=i+1
+	end
+	m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
