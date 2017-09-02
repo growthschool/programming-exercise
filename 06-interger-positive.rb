51403d6067f5004667825b6b0732affd0629743b
rockyjia <jrk1987@gmail.com>
2017-09-02 23:26:56 +0800

    == if else

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -1,10 +1,21 @@
 # 题目: 输入一个数字 x，请判断是否正数、零或负数，以及是不是偶数
 
-
-print "请输入一个整数，然后按 Enter: "
+print '请输入一个整数，然后按 Enter: '
 x = gets
 
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+(
+if x.to_i > 0
+  puts '这个数是正数'
+  if x.to_i.even?
+    puts '这个数是偶数'
+  else
+    puts '这个数是奇数'
+  end
+elsif x.to_i == 0
+  puts '这个数是零'
+else
+  puts '这个数是负数'
+end
+)
