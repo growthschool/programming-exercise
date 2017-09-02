ab70cdd3f0940bbd945ca975202473d3d126fc89
rockyjia <jrk1987@gmail.com>
2017-09-02 23:52:09 +0800

    if else if elseif else end end

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -8,15 +8,28 @@
 #     当 z > 0 输出 "D"
 #     当 z < 0 输出 "E"
 
-print "请输入一个整数x，然后按 Enter: "
+print '请输入一个整数x，然后按 Enter: '
 x = gets
 
-print "请输入一个整数y，然后按 Enter: "
+print '请输入一个整数y，然后按 Enter: '
 y = gets
 
-print "请输入一个整数z，然后按 Enter: "
+print '请输入一个整数z，然后按 Enter: '
 z = gets
 
 # ....
+if x.to_i < 0
+  puts 'A'
+else
+  if (x.to_i > 0) && (y.to_i > 0) && (z.to_i > 0)
+    puts 'B'
+  elsif (x.to_i > 0) && (y.to_i > 0) && z.to_i < 0
+    puts 'C'
+  else
+    puts 'D' if (y.to_i < 0) && (z.to_i > 0)
+    puts 'E' if (y.to_i < 0) && (z.to_i < 0)
 
-puts "结果是________(A或B或C或D或E)"
+end
+
+  puts '0' if x.to_i == 0
+end
