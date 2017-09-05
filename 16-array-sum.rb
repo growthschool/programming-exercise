632857c4dca5ae23c8da9da288ce31116c6e5bd5
rockyjia <jrk1987@gmail.com>
2017-09-05 22:09:48 +0800

    16 for end

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,11 +1,13 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  #....
+  x = 0
+  for i in array
+    x = i if x < i
+  end
+  x
 end
-
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
-
