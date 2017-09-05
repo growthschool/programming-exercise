1e5f3285e7376251caa1729a3d27732c6c33508b
escray <zescray@gmail.com>
2017-09-05 22:18:30 +0800

    13-nn.rb

--- 13-nn.rb
+++ 13-nn.rb
@@ -1,11 +1,13 @@
 # 题目: 输入一个数字 N，输出 N * N 乘法表
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
-
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
-
+n = gets.to_i
+i = 0
+while  i <= n
+  j = 0
+  while  j <= n
+    puts "#{i} × #{j} = #{i*j}"
+    j = j + 1
+  end
+  i = i + 1
+end
