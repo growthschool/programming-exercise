800a065ca5ffac38e222153330b942b202e2a59e
escray <zescray@gmail.com>
2017-09-06 19:58:06 +0800

    17-array-stats.rb

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -13,3 +13,8 @@ arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
 max = find_max(arr)
 puts "Max is #{max}" # 应该是 88
+
+arr.each_with_index do |i, j|
+  puts i
+  puts j
+end
--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -1,9 +1,9 @@
 # 使用者不断输入数字存进 Array，最后输出总和、平均、最大值、最小值
 
 arr = []
-
-while (true)
-  print "请输入数字，结束请直接按 Enter: "
+sum = 0
+loop do
+  print '请输入数字，结束请直接按 Enter: '
   user_input = gets
   if user_input == "\n"
     break
@@ -14,7 +14,13 @@ end
 
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+arr.each do |i|
+  sum += i.to_i
+end
+
+avr = (sum / arr.length).to_f
+
+puts "总和是 #{sum}"
+puts "平均是 #{avr}"
+puts "最大值是 #{arr.max}"
+puts "最小值是 #{arr.min}"
