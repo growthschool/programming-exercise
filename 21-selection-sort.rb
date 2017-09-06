710222b0a10a094bfee6363e949510c9ee3d8289
escray <zescray@gmail.com>
2017-09-06 21:44:32 +0800

    21-selection-sort.rb

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,19 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  min = 0
+  tmp = 0
+  j = 0
+  for i in 0..(arr.length - 1)
+    min = i
+    for j in (i + 1)..(arr.length - 1)
+      min = j if arr[min] > arr[j]
+    end
+    tmp = arr[min]
+    arr[min] = arr[i]
+    arr[i] = tmp
+  end
+  arr.to_s
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
