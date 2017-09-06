dd9a18ec2e738f32d89185459fe47f4a522f92b7
escray <zescray@gmail.com>
2017-09-06 22:04:42 +0800

    25-hash-count.rb

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -5,14 +5,18 @@ def count(arr)
 
   arr.each do |i|
     # ...
+    h[i] = if h.keys.include?(i)
+             h[i] + 1
+           else
+             1
+           end
   end
 
-  return h # 回传一个 hash
+  h # 回传一个 hash
 end
 
-arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a", "c", "e", "a", "d", "e"]
+arr =  %w(a d d c b c c c d d e e e d a c e a d e)
 
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
