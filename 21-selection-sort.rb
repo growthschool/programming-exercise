c2602a823956cfbdbdb8c8c69ae49bbcc6f2a62f
zhoujin <jin100chow@hotamil.com>
2017-09-07 15:38:36 +0800

    add time

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,24 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  temp = 0
+  i = 0
+  l = arr.size - 1
+  while ( i < l )
+    min = i
+    j = i + 1
+    while (j < arr.size )
+      if arr[min] > arr[j]
+        min = j
+      end
+      j += 1
+    end
+    temp = arr[min]
+    arr[min] = arr[i]
+    arr[i] = temp
+    i += 1
+  end
+  return arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
