4a40aadd3ce035c8751b5be2a0f069c57a8de058
rockyjia <jrk1987@gmail.com>
2017-09-07 22:36:15 +0800

    21 times do

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,8 +1,30 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
+# def selection_sort(arr)
+#   (0..arr.size - 1).each do |i|
+#     min = arr[i]
+#     k = i
+#     (i + 1..arr.size - 1).each do |j|
+#       if arr[j] < min
+#         min = arr[j]
+#         k = j
+#       end
+#     end
+#     arr[k] = arr[i]
+#     arr[i] = min
+#   end
+#   arr
+# end
+
 def selection_sort(arr)
-  #...
+  sorted = []
+  arr.size.times do |_i|
+    min = arr.min
+    sorted << min
+    arr.delete_at(arr.index(min))
+  end
+  sorted
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
