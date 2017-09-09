30ed9efc3b8e296cf598201810e4ddfd3f41aae1
rockyjia <jrk1987@gmail.com>
2017-09-10 00:11:00 +0800

    26 push() i[]

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -1,20 +1,23 @@
 # 给定一个数组包含 Hash，请过滤和排序
 
 arr = [
-  { "name" => "Peter", "age" => 30 },
-  { "name" => "John", "age" => 15 },
-  { "name" => "David", "age" => 45 },
-  { "name" => "Steven", "age" => 22 },
-  { "name" => "Vincent", "age" => 6 },
+  { 'name' => 'Peter', 'age' => 30 },
+  { 'name' => 'John', 'age' => 15 },
+  { 'name' => 'David', 'age' => 45 },
+  { 'name' => 'Steven', 'age' => 22 },
+  { 'name' => 'Vincent', 'age' => 6 }
 ]
 
-# ....
-
-puts "所有成年人，并由小到大: _________"
+answer = []
+arr.each do |i|
+  answer.push(i) if i['age'] >= 18
+end
+answer.sort_by { |k| k['age'] }
+puts "所有成年人，并由小到大: #{answer}"
 
 # 答案应该是
-#[
+# [
 #  { "name" => "Steven", "age" => 22 },
 #  { "name" => "Peter", "age" => 30 },
 #  { "name" => "David", "age" => 45 }
-#]
+# ]
