c778392099357a0dac686e3b03f9c73e8b51c107
silvia <gitz1594@icloud.com>
2017-09-23 13:41:37 +0800

    05-bmi.rb

--- 05-bmi.rb
+++ 05-bmi.rb
@@ -1,17 +1,28 @@
-# 题目: 输入体重和身高，输出身体质量指数(BMI)和建议
-# BMI 公式为 bmi = ( 体重 / (身高x身高) )，单位是公斤和米
-# 如果 BMI < 18.5，显示过轻
-# 如果 BMI >= 24，显示过重
-# 如果 BMI 介于 18.5 ~ 24，显示正常
+# 题目: 輸入體重和身高，輸出身體質量指数(BMI)和建議
+# BMI 公式為 bmi = ( 體重 / (身高x身高) )，單位是公斤和米
+# 如果 BMI < 18.5，顯示過輕
+# 如果 BMI >= 24，顯示過重
+# 如果 BMI 界于 18.5 ~ 24，顯示正常
 
-print "请输入您的体重(公斤)，然后按 Enter: "
+print "請輸入您的體重(公斤)，然後按 Enter: "
 weight = gets
 
-print "请输入您的身高(厘米)，然后按 Enter: "
+print "請輸入您的身高(厘米)，然後按 Enter: "
 height = gets
 
+bmi = (weight.to_f/(height.to_f/100 * height.to_f/100)).round(1)
+
+result =
+if bmi < 18.5
+  "過輕"
+elsif
+   bmi >= 24
+  "過重"
+else
+  "正常"
+end    
 # .....
 
-puts "您的 BMI 是: _________"
+puts "您的 BMI 是: #{bmi}"
 
-puts "您的 BMI 结果是: _________(过轻或正常或过重)"
+puts "您的 BMI 结果是: #{result}(過輕或正常或過重)"
