129388e74effb8b6771770fb431765f40a149e5f
silvia <gitz1594@icloud.com>
2017-09-23 16:09:13 +0800

    13-nn.rb

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,22 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
+n = n.to_i
+(1..n).each{|i|
+      (1..i).each{|j|
+        p = i * j
+        print j.to_s + '*' + i.to_s + '='
+        if p.to_s.length == 1 then
+          print p.to_s + '  '
+        else
+          print p.to_s + ' '
+        end
+      }
+      puts ''
+}
+
 # while ( ... )
 #   while ( ...)
 #
 #   end
 # end
-
