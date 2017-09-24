480c9319a2a3e73dedd80fb60f53576cb859f4dc
silvia <gitz1594@icloud.com>
2017-09-24 17:08:31 +0800

    29-todos.rb

--- 29-todos.rb
+++ 29-todos.rb
@@ -7,8 +7,14 @@ text.each_line do |line|
   todos << line.chomp
 end
 
-todos.each_with_index do |todo, index|
-  puts "#{index}: #{todo}"
+# todos.each_with_index do |todo, index|
+#   puts "#{index}: #{todo}"
+# end
+
+def show(array)
+  array.each_with_index do |todo, index|
+    puts "#{index}: #{todo}"
+  end
 end
 
 while (true)
@@ -17,17 +23,28 @@ while (true)
 
   if command == "add"
     print "请输入代办事项: "
-    # ...
+
+    todos << gets.chomp
+    show(todos)
+
   elsif command == "remove"
     print "请输入要删除的编号: "
-    # ...
+
+    n = gets.chomp.to_i
+    todos.delete_at(n)
+    show(todos)
+
   elsif command == "save"
     puts "存盘离开"
 
-    # ...
+    File.open("todos.txt", "w+") do |f|
+      todos.each do |t|
+        f << t + "\n"
+      end
+    end
+      
     break;
   else
     puts "看不懂，请再输入一次"
   end
 end
-
