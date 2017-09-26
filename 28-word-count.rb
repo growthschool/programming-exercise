1b49cc11e660d8ef074f2d8ce64087154a2cd2c8
Megy Chen <megy.chen203@gmail.com>
2017-09-26 22:54:50 +0800

    Commit 28-word-count.rb

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,18 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
 doc = File.read("wordcount.txt")
+word_count = Hash.new(0)
+doc.each_line do |line|
+  line = line.gsub(/[,'".]/,'')
+  words = line.split
+  words.each do |word|
+    word_count[word] += 1
+  end
+end
 
+puts '每个单词出现的频数为：'
+
+word_count.each do |key, value|
+  puts "#{key} #{value}"
+end
 # ...
