30690868e0ea9d31f8cce0eff8c6476410f3d1cc
Megy Chen <megy.chen203@gmail.com>
2017-09-26 23:25:01 +0800

    Commit 29-todos.rb

--- 29-todos.rb
+++ 29-todos.rb
@@ -14,16 +14,29 @@ end
 while (true)
   print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
   command = gets.chomp
-
   if command == "add"
     print "请输入代办事项: "
+    task = gets.chomp
+    todos << task
+    todos.each_with_index do |todo, index|
+      puts "#{index}: #{todo}"
+    end
     # ...
   elsif command == "remove"
     print "请输入要删除的编号: "
+    todos.delete_at(gets.chomp.to_i)
+    todos.each_with_index do |todo, index|
+      puts "#{index}: #{todo}"
+    end
     # ...
   elsif command == "save"
     puts "存盘离开"
-
+    File.open("todos.txt", "w+") do |f|
+      todos.each do |todo|
+        f << todo
+        f << "\n"
+      end
+    end
     # ...
     break;
   else
