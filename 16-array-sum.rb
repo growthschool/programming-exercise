03ca4333d4f3d9508512385e1184ad214fcaba8e
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2017-09-27 15:19:06 +0800

    array-sum

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,11 +1,10 @@
 # 给定一阵列内含数字，输出最大值
 
-def find_max(array)
-  #....
+def find_max(arr)
+  array = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88].max
 end
 
-arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
 
-max = find_max(arr)
+
+max = find_max(Array)
 puts "Max is #{max}" # 应该是 88
-
