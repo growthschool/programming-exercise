2aeb178191078873bbc19faadb7882b4ce62a2d9
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2017-09-29 19:56:05 +0800

    hash-count

--- 25-hash-count.rb
+++ 25-hash-count.rb
@@ -3,16 +3,20 @@
 def count(arr)
   h = {}
 
-  arr.each do |i|
-    # ...
+    arr.each do |i|
+      key = i.to_sym
+      if count = h[key]
+        h[key] = count + 1
+      else
+        h[key] = 1
+      end
+    end
+    return h # 回传一个 hash
   end
 
-  return h # 回传一个 hash
-end
 
 arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a", "c", "e", "a", "d", "e"]
 
 answer = count(arr)
 
 puts answer # 答案应该是 {"a"=>3, "d"=>6, "c"=>5, "b"=>1, "e"=>5}
-
