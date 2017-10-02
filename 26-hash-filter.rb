3f92e197cdc37050ec5b493204d8e2ee438a4e22
sarah <smile-wu@hotmail.com>
2017-10-02 21:25:42 +0800

    complete 26-hash-filter.rb

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,9 +8,29 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
-# ....
+def filter(arr)
+  x = []
+  arr.each do |i|
+    if i["age"] >= 18
+      x << i
+    end
+  end
 
-puts "所有成年人，并由小到大: _________"
+  n = x.length-1
+  loop do
+    swapped = false
+    n.times do |s|
+      if (x[s]["age"]) > (x[s+1]["age"])
+        x[s], x[s+1] = x[s+1], x[s]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  x
+end
+
+puts "所有成年人，并由小到大: #{filter(arr).to_s} "
 
 # 答案应该是
 #[
