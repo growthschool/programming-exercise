42e1910ea2a7e27e27b15c3f640a0649e7bf1bfd
fengkai <fengkaiwater@gmail.com>
2017-10-04 23:06:35 +0800

    24-hash-even.rb

--- 24-hash-even.rb
+++ 24-hash-even.rb
@@ -2,7 +2,17 @@
 
 def find_even_keys(hash)
 
-  # ... (请回传一个数组)
+  result1 = []
+  h = hash.values
+  for i in h
+    result1 << i if i % 2 == 0
+  end
+
+  result2 = []
+  for j in result1
+    result2 << hash::key(j)
+  end
+  result2
 
 end
 
@@ -17,5 +27,3 @@ h = {
 answer = find_even_keys(h)
 
 puts "有偶数 value 的 keys 有 #{answer}" # 应该是数组 [b,d,e]
-
-
