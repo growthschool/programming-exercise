9e9fccee26b585b7783fef0408d0e35bba155e36
fengkai <fengkaiwater@gmail.com>
2017-10-04 21:51:31 +0800

    21-selection-sort

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,12 +1,30 @@
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
-def selection_sort(arr)
-  #...
+# def selection_sort(arr)
+#   result = []
+#   arr.each do |i|
+#
+#   end
+# end
+#
+# arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
+#
+# answer = selection_sort(arr)
+#
+# puts answer.to_s # 应该是 [1, 7, 9, 42, 46, 46, 68, 77, 86, 91]
+
+
+arr = [4,1,2,5,7,10,3]
+
+for i in (0..arr.size-1)
+  for j in (i+1)..(arr.size-1)
+    if arr[j] < arr[i]
+      tmp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = tmp
+    end
+  end
 end
 
-arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
-
-answer = selection_sort(arr)
-
-puts answer.to_s # 应该是 [1, 7, 9, 42, 46, 46, 68, 77, 86, 91]
+puts(arr.inspect)  # inspect also aliased as: to_s
