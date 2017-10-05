352b355ebc45cafea9daf716a51492bb83bbef9d
ypj <ypj2005@126.com>
2017-10-05 19:49:17 +0800

    07-abcde

--- 07-abcde.rb
+++ 07-abcde.rb
@@ -18,5 +18,43 @@ print "请输入一个整数z，然后按 Enter: "
 z = gets
 
 # ....
+=begin
+t1 = x.to_i
+t2 = y.to_i
+t3 = z.to_i
 
-puts "结果是________(A或B或C或D或E)"
+if t1 < 0
+  s = "A"
+elsif t1 > 0 && t2 > 0 && t3 > 0
+  s = "B"
+elsif t1 > 0 && t2 > 0 && t3 < 0
+  s = "C"
+elsif t1 > 0 && t2 < 0 && t3 > 0
+  s = "D"
+else t1 >0 && t2 < 0 && t3 < 0
+  s = "E"
+end
+=end
+
+x = x.to_i
+y = y.to_i
+z = z.to_i
+
+if x < 0
+  s = "A"
+elsif x > 0 && y > 0
+  if z > 0
+    s = "B"
+  elsif z < 0
+    s = "C"
+  end
+elsif x > 0 && y < 0
+  if z > 0
+    s = "D"
+  elsif z < 0
+    s = "E"
+  end
+end
+
+
+puts "结果是: #{s} (A或B或C或D或E)"
