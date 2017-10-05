d2a6af14c576132adce66803bf462052a01d5873
fengkai <fengkaiwater@gmail.com>
2017-10-05 22:43:34 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,13 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+words = doc.split   # words = doc.split(" ")
+
+words_count = {}
+
+for w in words
+  words_count[w] ||= 0
+  words_count[w] += 1
+end
+
+puts(words_count)
