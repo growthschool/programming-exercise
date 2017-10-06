ae84f29eeef59d07180fb70c271353da2d086956
Planck1043 <zhao1036713209@gmail.com>
2017-10-06 22:03:16 +0800

    13 nn 第二种解法

--- 13-nn.rb
+++ 13-nn.rb
@@ -4,11 +4,12 @@ print "请输入数字 N，然后按 Enter: "
 n = gets.to_i
 i = 1
 
-while ( i <= n )
-  h = 1
-  while ( h <= n )
-    puts "#{i} * #{h} = #{i*h}"
-    h += 1
+x = 0
+while x <= n
+  y = 0
+  while y <= n
+    puts "#{x} x #{y} = #{x*y}"
+    y += 1
   end
-  i += 1
+  x += 1
 end
