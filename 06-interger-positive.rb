0877000913d80c44fc5662db0e581b5033f779c9
Planck1043 <zhao1036713209@gmail.com>
2017-10-06 21:37:18 +0800

    06 interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -2,9 +2,26 @@
 
 
 print "请输入一个整数，然后按 Enter: "
-x = gets
+x = gets.to_i
 
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+print "这个数是: "
+"#{
+if x > 0
+  puts "正数"
+elsif x == 0
+  puts "零"
+else x < 0
+  puts "负数"
+end
+}"
+
+print "这个数是: "
+"#{
+if x%2 == 0
+  puts "偶数"
+else
+  puts "奇数"
+end
+}"
