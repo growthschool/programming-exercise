b027e7e4eb4dcdc756ce47532ebbd2790824cbda
陈彦锋 <chenyanfeng@chenyanfengdeMacBook-Pro.local>
2017-10-17 21:54:22 +0800

    06-interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -4,7 +4,23 @@
 print "请输入一个整数，然后按 Enter: "
 x = gets
 
-# ....
+y = x.to_i
+z = y % 2
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if y == 0
+  result_1 = "零"
+elsif y < 0
+  result_1 = "负数"
+elsif y > 0
+  result_1 = "正数"
+end
+
+if z == 0
+  result_2 = "偶数"
+else
+  result_2 = "奇数"
+end
+
+
+puts "这个数是#{result_1}"
+puts "这个数是#{result_2}"
