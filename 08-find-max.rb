369ffefea0840053e4aee92ddf432690b0184a62
chentianwei <ctw.3@163.com>
2018-01-01 15:33:47 +0800

    08

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -9,12 +9,32 @@ y = (gets).to_i
 print "请输入一个数字z，然后按 Enter: "
 z = (gets).to_i
 
-if x > y && x > z
-  result = "x"
-elsif y > x && y > z
-  result = "y"
+if x > y
+  if x > z
+    result = "x"
+  elsif x < z
+    result = "z"
+  else
+    result = "x和z 并列最大"
+  end
+elsif x = y
+  if x > z
+    result = "x和y 并列最大"
+  elsif x < z
+    result = "z"
+  else
+    result = "x y z 一样大"
+  end
 else
-  result = "z"
+  if x >= z
+    result = "Y"
+  elsif y > z
+    result = "Y"
+  elsif y < z
+    result = "z"
+  else
+    result = "y和z 并列最大"
+  end
 end
 
-puts "最大的数是 ________(x或y或z)"
+puts "最大的数是 #{result}  (x或y或z)"
