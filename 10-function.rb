edb43aead78f6c65f5c2978534eb5b8cd7f5ad1a
chentianwei <ctw.3@163.com>
2018-01-01 16:29:25 +0800

    10

--- 10-function.rb
+++ 10-function.rb
@@ -1,16 +1,43 @@
 # 题目: 使用者输入 x,y,z，请输出三个数中最大的数
 
 def find_max(x, y, z)
+  if x > y
+    if x > z
+      return result = "x"
+    elsif x < z
+      return result = "z"
+    else
+      return result = "x和z 并列最大"
+    end
+  elsif x = y
+    if x > z
+      return result = "x和y 并列最大"
+    elsif x < z
+      return result = "z"
+    else
+      return result = "x y z 一样大"
+    end
+  else
+    if x >= z
+      return result = "Y"
+    elsif y > z
+      return result = "Y"
+    elsif y < z
+      return result = "z"
+    else
+      result = "y和z 并列最大"
+    end
+  end
 end
 
 print "请输入一个数字x，然后按 Enter: "
-x = gets
+x = (gets).to_f
 
 print "请输入一个数字y，然后按 Enter: "
-y = gets
+y = (gets).to_f
 
 print "请输入一个数字z，然后按 Enter: "
-z = gets
+z = (gets).to_f
 
 # ....
 
