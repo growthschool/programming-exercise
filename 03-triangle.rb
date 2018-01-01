888e78d7edc017d75a4d07f7a8e0d7feca65ec3c
chentianwei <ctw.3@163.com>
2018-01-01 16:18:06 +0800

    03

--- 03-triangle.rb
+++ 03-triangle.rb
@@ -1,11 +1,11 @@
 # 题目: 使用者输入直角三角形的宽和高，输出三角形的面积
 
 print "请输入直角三角形的高，然后按 Enter: "
-a = gets
+a = (gets).to_i
 
 print "请输入直角三角形的底边，然后按 Enter: "
-b = gets
-c = a.to_i * b.to_i
+b = (gets).to_i
+c = (a*b)/2
 # .....
 
 puts "直角三角形的面积是: ___#{c}______"
