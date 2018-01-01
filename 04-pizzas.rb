0441f3e427307da79d5f9ebf621a21cd83d8d0a0
chentianwei <ctw.3@163.com>
2018-01-01 12:46:52 +0800

    04

--- 04-pizzas.rb
+++ 04-pizzas.rb
@@ -5,8 +5,8 @@ pizzas = gets
 
 print "请输入有多少人要吃，然后按 Enter: "
 people = gets
-left = pizzas.to_i - (pizzas.to_i / people.to_i)*people.to_i
-# .....
+#left = pizzas.to_i - (pizzas.to_i / people.to_i)*people.to_i
+left = pizzas.to_i % people.to_i
 
 puts "每人可分得几片: #{ pizzas.to_i / people.to_i}片"
 puts "还剩下几片: #{left}片"
