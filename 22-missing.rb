9ccb11c64c37244b45a711e470930c07df3fc413
chentianwei <ctw.3@163.com>
2018-01-03 22:20:04 +0800

    repair arr.each

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,24 +1,21 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  contrast = [0,1,2,3,4,5,6,7,8,9]
+  contrast = [1,2,3,4,5,6,7,8,9]
   result = []
-  c = 0
-  while c < contrast.size
-    a = 0
-    while a < arr.size
-      arr_have = false
-      if contrast[c] == arr[a]
+
+  contrast.each do |c|
+    arr_have = false
+
+    arr.each do |a|
+      if c == a
         arr_have = true
         break
       end
-      a += 1
     end
     if arr_have == false
-      result << contrast[c]
+      result << c
     end
-
-    c += 1
   end
 
   return result
