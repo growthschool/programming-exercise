f046f2c02bbd7f6d5a3cbf4e8ef64009a3447899
chentianwei <ctw.3@163.com>
2018-01-03 09:56:47 +0800

    21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,28 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  temp = []
+  i = 0
+  l = arr.size - 1
+  # 双循环：外面的循环，是按顺序从前向后依次提取值，直到倒数第二个结束。
+  # 嵌套的循环，是让外循环提取的值和后续的值进行对比。
+  # 所以两个循环的次数都是n-1
+  while i < l
+    min = i
+    j = i + 1
+    while j < arr.size
+      if arr[min] > arr[j]
+        min = j
+      end
+      j += 1
+    end
+    temp = arr[i]
+    arr[i] = arr[min]
+    arr[min] = temp
+
+    i += 1
+  end
+  return arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
