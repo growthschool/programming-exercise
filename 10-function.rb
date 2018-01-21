fe3f4091a170eea2cbbb85299b4a8b3cb988e2ce
amy326 <amy87326@163.com>
2018-01-21 20:17:10 +0800

    finished 10

--- 10-function.rb
+++ 10-function.rb
@@ -1,18 +1,24 @@
-# 题目: 使用者输入 x,y,z，请输出三个数中最大的数
+# 题目: 使用者输入 x,y,z，请输出三个数中最大的数。使用回传值表示
 
 def find_max(x, y, z)
+   if x > z && x > y
+      return x
+   elsif y > z && y > x
+      return y
+   elsif z > x && z > y
+      return z
+   end
 end
 
 print "请输入一个数字x，然后按 Enter: "
-x = gets
+x = gets.to_f
 
 print "请输入一个数字y，然后按 Enter: "
-y = gets
+y = gets.to_f
 
 print "请输入一个数字z，然后按 Enter: "
-z = gets
+z = gets.to_f
 
-# ....
 
 answer = find_max(x,y,z)
 
