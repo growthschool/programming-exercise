a3c67bf366250f6d0f173f7bd769992a989903b9
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-04-28 23:56:59 +0800

    13

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,19 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
+i = 0
 
+ while ( i.to_i < n.to_i )
+
+    puts "#{i.to_i} x #{i.to_i} = #{i * i}"
+    puts "#{i.to_i} x #{i.to_i + 1} = #{i * (i + 1)}"
+    puts "#{i.to_i + 1} x #{i.to_i} = #{i * (i + 1)}"
+
+    i += 1
+
+    while ( i.to_i == n.to_i )
+      puts "#{i.to_i} x #{i.to_i} = #{i * i}"
+      break
+    end
+
+  end
